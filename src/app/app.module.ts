9e62a32491f91d01297d03e08d79cb2e86073fab
biaogebusy <349255833@qq.com>
2020-12-11 00:04:28 +0800

    feat: add map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrandingModule } from './branding/branding.module';
+import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MapComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/branding/branding.module.ts
+++ src/app/branding/branding.module.ts
@@ -1,12 +1,31 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { DomSanitizer } from '@angular/platform-browser';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ShareModule } from '../share/share.module';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule, ShareModule],
-  exports: [HeaderComponent, FooterComponent],
+  imports: [ShareModule, BrowserAnimationsModule, FlexLayoutModule],
+  exports: [FlexLayoutModule, HeaderComponent, FooterComponent],
 })
-export class BrandingModule {}
+export class BrandingModule {
+  /**
+   * @SkipSelf 让模块去父级寻找依赖，不然会造成死循环
+   * @Optional 可选，如果CoreModule不存在正常执行
+   * @param parent
+   * @param iconRegistry
+   * @param ds
+   */
+  constructor(
+    @Optional() @SkipSelf() parent: BrandingModule,
+    ds: DomSanitizer
+  ) {
+    if (parent) {
+      throw new Error('Core 模块已经存在，不能再加载！');
+    }
+  }
+}
