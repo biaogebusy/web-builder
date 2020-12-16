5a49157523a0ca08f9fb959f38a7c828c7da107e
biaogebusy <349255833@qq.com>
2020-12-16 23:23:44 +0800

    feat: add service

---
+++ src/app/service/service.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ServiceService } from './service.service';
+
+describe('ServiceService', () => {
+  let service: ServiceService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ServiceService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
