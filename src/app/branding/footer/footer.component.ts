4d33e48f5500c439a18291b73c7029c9292a32ac
biaogebusy <349255833@qq.com>
2021-01-12 14:38:14 +0800

    update footer

--- src/app/branding/footer/footer.component.ts
+++ src/app/branding/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UtilitiesService } from '../../service/utilities.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public utilities: UtilitiesService
+  ) { }
 
   ngOnInit(): void {
   }
--- src/app/service/utilities.service.ts
+++ src/app/service/utilities.service.ts
@@ -10,4 +10,8 @@ export class UtilitiesService {
   getIndexTitle(title: string): string {
     return title.substring(0, 1);
   }
+
+  get fullYear(){
+    return new Date().getFullYear();
+  }
 }
