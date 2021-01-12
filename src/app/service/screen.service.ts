66c8f55370bd3ce9a289934e22a7ab68c676c36e
biaogebusy <349255833@qq.com>
2021-01-12 18:40:07 +0800

    feat: add screen state

--- src/app/service/screen.service.ts
+++ src/app/service/screen.service.ts
@@ -1,21 +1,60 @@
 import { Injectable } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { action, observable, computed } from 'mobx-angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScreenService {
+  @observable viewPort = 'xs';
+  @observable state = {
+    viewPort: this.viewPort
+  };
 
-  constructor() { }
+  constructor(
+    public breakpointObserver: BreakpointObserver
+  ) {
+    this.initScreen();
+  }
 
-  isElementInViewport (el: any) {
+  @action
+  initScreen(): any {
+    this.breakpointObserver.observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge
+    ]).subscribe((state: BreakpointState) => {
+      if (state.breakpoints[Breakpoints.XSmall]) {
+        this.viewPort = 'xs';
+      }
+      if (state.breakpoints[Breakpoints.Small]) {
+        this.viewPort = 'sm';
+      }
+      if (state.breakpoints[Breakpoints.Medium]) {
+        this.viewPort = 'md';
+      }
+      if (state.breakpoints[Breakpoints.Large]) {
+        this.viewPort = 'lg';
+      }
+      if (state.breakpoints[Breakpoints.XLarge]) {
+        this.viewPort = 'xl';
+      }
+    });
+  }
 
-    var rect = el.getBoundingClientRect();
+  eq(targetPoint: string): boolean {
+    return targetPoint === this.viewPort;
+  }
 
+  isElementInViewport(el: any): boolean {
+    const rect = el.getBoundingClientRect();
     return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
     );
-}
+  }
 }
