c54d8e90b2d2b71c04b856ef1ecd2e16f9dbb1fb
biaogebusy <349255833@qq.com>
2021-01-14 00:06:46 +0800

    update footer and screen

--- src/app/mobx/screen/ScreenState.service.ts
+++ src/app/mobx/screen/ScreenState.service.ts
@@ -1,22 +1,63 @@
 import { Injectable } from '@angular/core';
-import { Subject,fromEvent } from 'rxjs';
+import { Subject, fromEvent } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { action, observable, computed } from 'mobx-angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScreenState {
-  public scroll$ = new Subject;
+  public scroll$ = new Subject();
+  @observable viewPort = 'xs';
+  @observable state = {
+    viewPort: this.viewPort
+  };
 
-  constructor() {
-    this.initBrowserEvents()
+
+  constructor(
+    public breakpointObserver: BreakpointObserver
+  ) {
+    this.initScreen();
+    this.initBrowserEvents();
+  }
+
+  @action
+  initScreen(): any {
+    this.breakpointObserver.observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge
+    ]).subscribe((state: BreakpointState) => {
+      if (state.breakpoints[Breakpoints.XSmall]) {
+        this.viewPort = 'xs';
+      }
+      if (state.breakpoints[Breakpoints.Small]) {
+        this.viewPort = 'sm';
+      }
+      if (state.breakpoints[Breakpoints.Medium]) {
+        this.viewPort = 'md';
+      }
+      if (state.breakpoints[Breakpoints.Large]) {
+        this.viewPort = 'lg';
+      }
+      if (state.breakpoints[Breakpoints.XLarge]) {
+        this.viewPort = 'xl';
+      }
+    });
+  }
+
+  eq(targetPoint: string): boolean {
+    return targetPoint === this.viewPort;
   }
 
   initBrowserEvents(){
     const scroll = fromEvent(window, 'scroll').pipe(
       debounceTime(200)
-    ).subscribe(()=>{
+    ).subscribe(() => {
       this.scroll$.next();
-    })
+    });
   }
 }
--- src/app/service/screen.service.ts
+++ src/app/service/screen.service.ts
@@ -6,47 +6,10 @@ import { action, observable, computed } from 'mobx-angular';
   providedIn: 'root'
 })
 export class ScreenService {
-  @observable viewPort = 'xs';
-  @observable state = {
-    viewPort: this.viewPort
-  };
 
-  constructor(
-    public breakpointObserver: BreakpointObserver
-  ) {
-    this.initScreen();
+  constructor() {
   }
 
-  @action
-  initScreen(): any {
-    this.breakpointObserver.observe([
-      Breakpoints.XSmall,
-      Breakpoints.Small,
-      Breakpoints.Medium,
-      Breakpoints.Large,
-      Breakpoints.XLarge
-    ]).subscribe((state: BreakpointState) => {
-      if (state.breakpoints[Breakpoints.XSmall]) {
-        this.viewPort = 'xs';
-      }
-      if (state.breakpoints[Breakpoints.Small]) {
-        this.viewPort = 'sm';
-      }
-      if (state.breakpoints[Breakpoints.Medium]) {
-        this.viewPort = 'md';
-      }
-      if (state.breakpoints[Breakpoints.Large]) {
-        this.viewPort = 'lg';
-      }
-      if (state.breakpoints[Breakpoints.XLarge]) {
-        this.viewPort = 'xl';
-      }
-    });
-  }
-
-  eq(targetPoint: string): boolean {
-    return targetPoint === this.viewPort;
-  }
 
   isElementInViewport(el: any): boolean {
     const rect = el.getBoundingClientRect();
