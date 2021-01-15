7936df2f378cd47e80d919f3a975d292dfd38003
biaogebusy <349255833@qq.com>
2021-01-15 12:15:39 +0800

    fix: branding state

--- src/app/mobx/BrandingStare.ts
+++ src/app/mobx/BrandingStare.ts
@@ -3,6 +3,7 @@ import { action, observable, computed } from 'mobx-angular';
 import { first } from 'rxjs/operators';
 import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { ApiService } from '../service/api.service';
 
 
 @Injectable()
@@ -10,14 +11,15 @@ export class BrandingState {
 
   @observable public content: any;
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private apiService: ApiService
   ) {
     this.initBranding();
     }
 
     @action
     initBranding(): any {
-      this.http.get(`http://localhost:4200/assets/app/core/branding.json`).subscribe(branding => {
+      this.http.get(`${this.apiService.apiUrl}/assets/app/core/branding.json`).subscribe(branding => {
         this.content = branding;
       });
     }
