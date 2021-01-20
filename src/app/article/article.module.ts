480578f911e65af2395351ce48f4324b5f3f56c4
biaogebusy <349255833@qq.com>
2021-01-20 18:59:17 +0800

    feat: add article loading state

--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShareModule } from '../share/share.module';
 import { ArticleComponent } from './article.component';
+import { UiuxModule } from '../uiux/uiux.module';
 @NgModule({
   declarations: [ArticleComponent],
-  imports: [CommonModule, ShareModule],
+  imports: [CommonModule, ShareModule, UiuxModule],
 })
+
 export class ArticleModule {}
