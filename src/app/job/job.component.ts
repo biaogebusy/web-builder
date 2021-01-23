2a153d478af3dc45ad8f48f74b3831d0b19f2172
biaogebusy <349255833@qq.com>
2021-01-24 00:59:34 +0800

    fix: amap

--- src/app/job/job.component.ts
+++ src/app/job/job.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../service/api.service';
 import { NodeService } from '../service/node.service';
-import { forkJoin, Observable } from 'rxjs';
+import { forkJoin, Observable, Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { IJob, IChipList } from './IJob';
 import * as _ from 'lodash';
+import * as AMapLoader from '@amap/amap-jsapi-loader';
 @Component({
   selector: 'app-job',
   templateUrl: './job.component.html',
@@ -13,11 +14,17 @@ import * as _ from 'lodash';
 })
 export class JobComponent implements OnInit {
   nodes: any[];
-  relation: object = {};
-
+  relation: any;
+  included: any;
+  positon: object = {};
   regions: any;
   loading: boolean;
+  AMap: any;
+  geocoder: any;
+  city = '0771';
 
+  $position = new Subject();
+  show = false;
   constructor(
     private apiService: ApiService,
     private nodeService: NodeService
@@ -27,6 +34,11 @@ export class JobComponent implements OnInit {
 
   ngOnInit(): void {
     this.getJobsNodes();
+    this.$position.subscribe((state) => {
+      if (state) {
+        this.show = true;
+      }
+    });
   }
 
   getJobsNodes(): void {
@@ -46,15 +58,59 @@ export class JobComponent implements OnInit {
     this.nodeService.getNodes('job', params).subscribe((res) => {
       this.loading = false;
       this.nodes = res.data;
+      this.included = res.included;
       if (_.isArray(res.included)) {
         this.relation = _.keyBy(res.included, 'id');
-        console.log(this.relation);
+        this.initMap(res.included);
       }
     });
   }
 
+  initMap(included: any): void {
+    AMapLoader.load({
+      key: '33877188af64f0213ff4fa259b1c14b8',
+      version: '1.4.15',
+      plugins: ['AMap.Geocoder'], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
+      AMapUI: {
+        // 是否加载 AMapUI，缺省不加载
+        version: '1.1', // AMapUI 缺省 1.1
+        plugins: [], // 需要加载的 AMapUI ui插件
+      },
+      Loca: {
+        // 是否加载 Loca， 缺省不加载
+        version: '2.0', // Loca 版本，缺省 1.3.2
+      },
+    }).then((AMap: any) => {
+      this.AMap = AMap;
+
+      this.geocoder = new AMap.Geocoder({
+        city: this.city,
+      });
+      this.getPosition(included);
+    });
+  }
+
+  getPosition(included: any): void {
+    const companys = included.filter((item: any) => {
+      return item.type === 'node--company';
+    });
+    if (companys.length > 0) {
+      companys.forEach((item: any, index: number) => {
+        const address = item.attributes.address.address_line1;
+        this.geocoder.getLocation(address, (status: any, result: any) => {
+          if (status === 'complete' && result.info === 'OK') {
+            const location = result.geocodes[0].location;
+            this.relation[item.id].position = [location.lng, location.lat];
+            if (companys.length === index + 1) {
+              this.$position.next(true);
+            }
+          }
+        });
+      });
+    }
+  }
+
   regionFilter(event: any): void {
-    // console.log(event);
     if (event === 'all') {
       this.getJobsNodes();
     }
@@ -64,7 +120,6 @@ export class JobComponent implements OnInit {
   }
 
   getWelfare(lists: string[]): IChipList[] {
-    // console.log(lists)
     return lists
       .map((list) => {
         return {
--- src/app/job/map/map.component.ts
+++ src/app/job/map/map.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import * as AMapLoader from '@amap/amap-jsapi-loader';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -7,7 +6,9 @@ import * as AMapLoader from '@amap/amap-jsapi-loader';
 })
 export class MapComponent implements OnInit, OnDestroy {
   constructor() {}
-
+  @Input() content: any;
+  @Input() relation: any;
+  @Input() AMap: any;
   map: any;
   zoom = 13; // PC上，参数zoom可设范围：[3,18]；
   center = [108.32067, 22.817424];
@@ -19,77 +20,44 @@ export class MapComponent implements OnInit, OnDestroy {
     // https://lbs.amap.com/api/jsapi-v2/summary
     // https://lbs.amap.com/api/javascript-api/guide/layers/official-layers#default
     // https://lbs.amap.com/console/show/picker
-    AMapLoader.load({
-      key: '33877188af64f0213ff4fa259b1c14b8',
-      version: '1.4.15',
-      plugins: [], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
-      AMapUI: {
-        // 是否加载 AMapUI，缺省不加载
-        version: '1.1', // AMapUI 缺省 1.1
-        plugins: [], // 需要加载的 AMapUI ui插件
-      },
-      Loca: {
-        // 是否加载 Loca， 缺省不加载
-        version: '2.0', // Loca 版本，缺省 1.3.2
-      },
-    })
-      .then((AMap: any) => {
-        this.map = new AMap.Map('map', {
-          zoom: this.zoom,
-          center: this.center,
-          mapStyle: this.mapStyle,
-          features: this.features,
-        });
-        const company = [
-          {
-            name: '戴文科技',
-            logo:
-              'https://job-sit.zhaobg.com/sites/default/files/2021-01/CgqKkVePQ7SAK8dOAABQUAlvNu8191.png',
-            lal: [108.278416, 22.876778],
-          },
-          {
-            name: '北部湾港',
-            logo:
-              'https://job-sit.zhaobg.com/sites/default/files/2021-01/CgqKkVePQ7SAK8dOAABQUAlvNu8191.png',
-            lal: [108.389066, 22.758161],
-          },
-          {
-            name: '掌悦网络科技',
-            logo:
-              'https://job-sit.zhaobg.com/sites/default/files/2021-01/CgqKkVePQ7SAK8dOAABQUAlvNu8191.png',
-            lal: [108.395291, 22.810706],
-          },
-          {
-            name: '佳凯智能技术',
-            logo:
-              'https://job-sit.zhaobg.com/sites/default/files/2021-01/CgqKkVePQ7SAK8dOAABQUAlvNu8191.png',
-            lal: [108.27455, 22.865537],
-          },
-        ];
-        const markers = company.map((marker) => {
-          return new AMap.Marker({
-            content: `
+    this.map = new this.AMap.Map('map', {
+      zoom: this.zoom,
+      center: this.center,
+      mapStyle: this.mapStyle,
+      features: this.features,
+    });
+
+    const markers = this.content.map((marker: any) => {
+      return new this.AMap.Marker({
+        content: `
             <div class="mark-card p-y-xs p-x-xs">
               <div class="media m-right-xs">
-                <img src="${marker.logo}" />
+                <img src="${
+                  this.relation[
+                    this.relation[marker.relationships.company.data.id]
+                      .relationships.logo.data.id
+                  ].attributes.uri.url
+                }" />
               </div>
               <div class="media-body">
-                <div class="mat-h3 m-bottom-xs text-base">${marker.name}</div>
-                <div class="mat-h3 m-bottom-xs text-dark">前端开发</div>
-                <div class="mat-h3 m-bottom-0 text-primary">8 - 12k</div>
+                <div class="mat-h3 m-bottom-xs text-base">${
+                  this.relation[marker.relationships.company.data.id].attributes
+                    .title
+                }</div>
+                <div class="mat-h3 m-bottom-xs text-dark">${
+                  marker.attributes.title
+                }</div>
+                <div class="mat-h3 m-bottom-0 text-primary">${
+                  marker.attributes.salary.from
+                } -
+          ${marker.attributes.salary.to} k</div>
               </div>
             </div>`,
-            position: marker.lal,
-            title: marker.name,
-          });
-        });
-        console.log(markers);
-        this.map.add(markers);
-        console.log('center:', this.map.getCenter());
-      })
-      .catch((e: any) => {
-        console.log(e);
+        position: this.relation[marker.relationships.company.data.id].position,
+        title: marker.attributes.title,
       });
+    });
+    this.map.add(markers);
   }
 
   ngOnDestroy(): void {
