59c34280006ae4bf1bd67ab4b26701868333bf70
biaogebusy <349255833@qq.com>
2021-02-04 15:54:10 +0800

    fix: search input event

--- src/app/job/job-filter/job-filter.component.ts
+++ src/app/job/job-filter/job-filter.component.ts
@@ -1,19 +1,39 @@
-import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
+import { Component, Input, Output, OnInit, EventEmitter, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
+import { map, debounceTime, distinctUntilChanged, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-job-filter',
   templateUrl: './job-filter.component.html',
   styleUrls: ['./job-filter.component.scss'],
 })
-export class JobFilterComponent implements OnInit {
+export class JobFilterComponent implements OnInit, AfterViewInit {
+
   @Input() content: any;
   @Output() regionChange = new EventEmitter();
-  selectedValue: string = '';
-  constructor() {}
+  selectedValue = '';
+  @ViewChild('search', { read: ElementRef }) search: ElementRef;
+  subscription: Subscription;
 
-  ngOnInit(): void {}
+  constructor(
+  ) { }
 
-  onSelectRegion(event: any) {
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    const $input = fromEvent<any>(this.search.nativeElement, 'input').pipe(
+      map(event => event.target.value),
+      startWith(''),
+      debounceTime(200),
+      distinctUntilChanged()
+    );
+
+    this.subscription = $input.subscribe(key => {
+      console.log(key);
+    });
+  }
+  onSelectRegion(event: any): void {
     console.log(event);
     this.regionChange.emit(event.value);
   }
