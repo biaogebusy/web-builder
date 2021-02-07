33e1fa67e3c017f6dde4507a68fdefee63f489b8
biaogebusy <349255833@qq.com>
2021-02-07 12:21:26 +0800

    feat: add gotoTop

--- src/app/branding/footer/footer.component.ts
+++ src/app/branding/footer/footer.component.ts
@@ -1,23 +1,53 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  Inject,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+} from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { UtilitiesService } from '../../service/utilities.service';
 import { MatAccordion } from '@angular/material/expansion';
 import { BrandingState } from '../../mobx/BrandingStare';
 import { ScreenState } from '../../mobx/screen/ScreenState';
-
+import { ScreenService } from 'src/app/service/screen.service';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, AfterViewInit {
   content: any;
   panelOpenState = false;
+
   @ViewChild(MatAccordion) accordion: MatAccordion;
+  @ViewChild('goTop') goTop: ElementRef;
   constructor(
     public utilities: UtilitiesService,
     public screen: ScreenState,
-    public branding: BrandingState
+    private screenService: ScreenService,
+    public branding: BrandingState,
+    @Inject(DOCUMENT) private document: Document
   ) {}
 
   ngOnInit(): void {}
+
+  gotoTop(): void {
+    this.screenService.gotoTop();
+  }
+
+  onScroll(): void {
+    this.screen.scroll$.subscribe(() => {
+      if (this.document.body.getBoundingClientRect().top < -100) {
+        this.goTop.nativeElement.style.bottom = '2rem';
+      } else {
+        this.goTop.nativeElement.style.bottom = '-10rem';
+      }
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.onScroll();
+  }
 }
--- src/app/service/screen.service.ts
+++ src/app/service/screen.service.ts
@@ -1,28 +1,33 @@
 import { Injectable, ElementRef } from '@angular/core';
-import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import {
+  BreakpointObserver,
+  Breakpoints,
+  BreakpointState,
+} from '@angular/cdk/layout';
 import { action, observable, computed } from 'mobx-angular';
 import { Element } from '@angular/compiler';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ScreenService {
-
-  constructor() {
-  }
-
+  constructor() {}
 
   isElementInViewport(el: any): boolean {
     const rect = el.getBoundingClientRect();
     return (
       rect.top >= 0 &&
       rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+      rect.bottom <=
+        (window.innerHeight ||
+          document.documentElement.clientHeight) /* or $(window).height() */ &&
+      rect.right <=
+        (window.innerWidth ||
+          document.documentElement.clientWidth) /* or $(window).width() */
     );
   }
 
-  isElementOutTopViewport(el: any): boolean{
+  isElementOutTopViewport(el: any): boolean {
     const rect = el.getBoundingClientRect();
     return (
       rect.top < -el.clientHeight &&
@@ -30,10 +35,18 @@ export class ScreenService {
     );
   }
 
-  isElementOutBottomViewport(el: any): boolean{
+  isElementOutBottomViewport(el: any): boolean {
     const rect = el.getBoundingClientRect();
     return (
       rect.top > (window.innerHeight || document.documentElement.clientHeight)
     );
+  }
+
+  gotoTop(): void {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
   }
 }
