fcb2901a8b9852ab7ddf5be37920515ba1070442
biaogebusy <349255833@qq.com>
2021-03-31 21:57:58 +0800

    fix: case node

--- src/app/uiux/widgets/swiper/swiper.component.ts
+++ src/app/uiux/widgets/swiper/swiper.component.ts
@@ -31,27 +31,27 @@ export class SwiperComponent implements OnInit {
     navigation: navigationConfig,
     pagination: paginationgConfig,
     // Responsive breakpoints
-    breakpoints: {
-      // when window width is >= 320px
-      320: {
-        slidesPerView: 1.2,
-        spaceBetween: 10,
-      },
-      // when window width is >= 480px
-      480: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
-      // when window width is >= 640px
-      640: {
-        slidesPerView: 3,
-        spaceBetween: 30,
-      },
-      768: {
-        slidesPerView: 4,
-        spaceBetween: 30,
-      },
-    },
+    // breakpoints: {
+    //   // when window width is >= 320px
+    //   320: {
+    //     slidesPerView: 1.2,
+    //     spaceBetween: 10,
+    //   },
+    //   // when window width is >= 480px
+    //   480: {
+    //     slidesPerView: 2,
+    //     spaceBetween: 20,
+    //   },
+    //   // when window width is >= 640px
+    //   640: {
+    //     slidesPerView: 3,
+    //     spaceBetween: 30,
+    //   },
+    //   768: {
+    //     slidesPerView: 4,
+    //     spaceBetween: 30,
+    //   },
+    // },
   };
   config: SwiperConfigInterface;
   ngOnInit(): void {
