40283d85c43eacac90322e2973d2201d9f162135
biaogebusy <349255833@qq.com>
2021-04-14 22:01:42 +0800

    fix: video bg

--- src/app/uiux/combs/video/video-bg/video-bg.component.ts
+++ src/app/uiux/combs/video/video-bg/video-bg.component.ts
@@ -1,38 +1,48 @@
-import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild, ViewEncapsulation, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+  ViewEncapsulation,
+  AfterViewInit,
+} from '@angular/core';
 import videojs from 'video.js';
 @Component({
   selector: 'app-video-bg',
   templateUrl: './video-bg.component.html',
-  styleUrls: ['./video-bg.component.scss']
+  styleUrls: ['./video-bg.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class VideoBgComponent implements OnInit, AfterViewInit {
+export class VideoBgComponent implements OnInit, AfterViewInit, OnDestroy {
   @Input() content: any;
   @ViewChild('target', { static: true }) target: ElementRef;
   // see options: https://github.com/videojs/video.js/blob/maintutorial-options.html
   // https://docs.videojs.com/tutorial-angular.html
-  @Input() options: {
-    fluid: boolean,
-    aspectRatio: string,
-    autoplay: boolean,
+  options: {
+    fluid: boolean;
+    aspectRatio: string;
+    autoplay: boolean;
     sources: {
-      src: string,
-      type: string,
-    }[],
+      src: string;
+      type: string;
+    }[];
   };
   player: videojs.Player;
-  constructor(
-    private elementRef: ElementRef,
-  ) { }
-  ngOnInit(): void { }
+  constructor(private elementRef: ElementRef) {}
+  ngOnInit(): void {}
   ngAfterViewInit(): void {
+    this.options = this.content.params;
     // instantiate Video.js
-    this.player = videojs(this.target.nativeElement, this.options, function onPlayerReady() {
-      // console.log('onPlayerReady', this);
+    console.log(this.elementRef);
+    const video = this.elementRef.nativeElement.querySelectorAll('video')[0];
+    this.player = videojs(video, this.options, function onPlayerReady() {
+      console.log('onPlayerReady');
     });
   }
 
   ngOnDestroy() {
-    // destroy player
     if (this.player) {
       this.player.dispose();
     }
