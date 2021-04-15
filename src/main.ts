c333d70c24f2665e66352d3f20fe71a18a39dc7d
biaogebusy <349255833@qq.com>
2021-04-15 15:48:04 +0800

    rm hammerjs

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import 'hammerjs';
+// import 'hammerjs';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
