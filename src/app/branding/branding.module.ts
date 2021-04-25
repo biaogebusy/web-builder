cfe5d20556748e99445d93c7c2618b45e99a27f7
biaogebusy <349255833@qq.com>
2021-04-25 15:21:17 +0800

    quick fix

--- src/app/branding/branding.module.ts
+++ src/app/branding/branding.module.ts
@@ -5,7 +5,6 @@ import { ShareModule } from '../share/share.module';
 import { MenuComponent } from './header/menu/menu.component';
 import { UserMenuComponent } from './header/menu/user-menu/user-menu.component';
 import { SwitchThemeComponent } from './header/menu/switch-theme/switch-theme.component';
-import { UiuxModule } from '../uiux/uiux.module';
 import { PopupComponent } from './footer/popup/popup.component';
 import { ItemMenuComponent } from './header/menu/item-menu/item-menu.component';
 import { SubMenuComponent } from './header/menu/item-menu/sub-menu/sub-menu.component';
