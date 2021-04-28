172bdd5dd1dce82e0097a21774ee7eb85db41d08
biaogebusy <349255833@qq.com>
2021-04-28 18:27:03 +0800

    feat: add tab,panel

--- src/app/uiux/widgets/widgets.module.ts
+++ src/app/uiux/widgets/widgets.module.ts
@@ -1,29 +1,33 @@
 import { NgModule } from '@angular/core';
-import { ShareModule } from '../../share/share.module';
-import { MatChipsModule } from '@angular/material/chips';
-import { ChipListComponent } from './chip-list/chip-list.component';
-import { SpinnerComponent } from './spinner/spinner.component';
-import { BgComponent } from './bg/bg.component';
-import { LinkComponent } from './link/link.component';
-import { CardComponent } from './card/card.component';
-import { SwiperComponent } from './swiper/swiper.component';
-import { ImgComponent } from './img/img.component';
-import { DynamicWidgetsComponent } from './dynamic-widgets/dynamic-widgets.component';
+
 import { SwiperModule } from 'ngx-swiper-wrapper';
 import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+import { ShareModule } from '../../share/share.module';
+import { MatChipsModule } from '@angular/material/chips';
 import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+
+import { BgComponent } from './bg/bg.component';
+import { ImgComponent } from './img/img.component';
+import { BoxComponent } from './box/box.component';
+import { MapComponent } from './map/map.component';
+import { TabComponent } from './tab/tab.component';
+import { LinkComponent } from './link/link.component';
+import { CardComponent } from './card/card.component';
+import { TextComponent } from './text/text.component';
 import { TitleComponent } from './title/title.component';
-import { DropdownMenuComponent } from './dropdown-menu/dropdown-menu.component';
-import { NumberAnimateComponent } from './number-animate/number-animate.component';
+import { PanelComponent } from './panel/panel.component';
+import { SwiperComponent } from './swiper/swiper.component';
+import { SpacerComponent } from './spacer/spacer.component';
+import { SpinnerComponent } from './spinner/spinner.component';
+import { ChipListComponent } from './chip-list/chip-list.component';
+import { LineYearComponent } from './line-year/line-year.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 import { BtnAnimateComponent } from './btn-animate/btn-animate.component';
 import { FeatureBoxComponent } from './feature-box/feature-box.component';
-import { BoxComponent } from './box/box.component';
-import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
-import { MapComponent } from './map/map.component';
 import { TestimonialComponent } from './testimonial/testimonial.component';
-import { LineYearComponent } from './line-year/line-year.component';
-import { TextComponent } from './text/text.component';
-import { SpacerComponent } from './spacer/spacer.component';
+import { DropdownMenuComponent } from './dropdown-menu/dropdown-menu.component';
+import { NumberAnimateComponent } from './number-animate/number-animate.component';
+import { DynamicWidgetsComponent } from './dynamic-widgets/dynamic-widgets.component';
 
 const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
   direction: 'horizontal',
@@ -37,8 +41,10 @@ const components = [
   BoxComponent,
   TextComponent,
   LinkComponent,
+  TabComponent,
   CardComponent,
   TitleComponent,
+  PanelComponent,
   SwiperComponent,
   SpacerComponent,
   SpinnerComponent,
