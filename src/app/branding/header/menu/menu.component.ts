cf2c85618b3aa2fde2bbf7193e0d843ce8f51a65
biaogebusy <349255833@qq.com>
2021-04-29 17:54:01 +0800

    feat: update version

--- src/app/branding/header/menu/menu.component.ts
+++ src/app/branding/header/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ScreenState } from '../../../mobx/screen/ScreenState';
 import { BrandingState } from '../../../mobx/BrandingStare';
+import { AppState } from '../../../mobx/AppState';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -11,7 +12,7 @@ export class MenuComponent implements OnInit {
   @Input() isDrawer: boolean;
   @Input() content: any;
 
-  constructor(public screen: ScreenState) {}
+  constructor(public screen: ScreenState, public appState: AppState) {}
 
   ngOnInit(): void {}
 
