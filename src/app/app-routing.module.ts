6c2bcebc657787d62498070000682fd3c45dce47
biaogebusy <349255833@qq.com>
2021-05-23 15:38:02 +0800

    update router for home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'lists/jobs',
+    redirectTo: 'home',
     pathMatch: 'full',
   },
   {
