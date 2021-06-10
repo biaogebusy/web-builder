3c1f741e3511642deab7250c88eaae9665a2e844
biaogebusy <349255833@qq.com>
2021-06-11 00:25:42 +0800

    feat: showcase 2v2

---
+++ src/app/uiux/combs/showcase/showcase2v2/showcase2v2.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Showcase2v2Component } from './showcase2v2.component';
+
+describe('Showcase2v2Component', () => {
+  let component: Showcase2v2Component;
+  let fixture: ComponentFixture<Showcase2v2Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ Showcase2v2Component ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Showcase2v2Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/uiux/combs/showcase/showcase2v2/showcase2v2.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-showcase2v2',
+  templateUrl: './showcase2v2.component.html',
+  styleUrls: ['./showcase2v2.component.scss'],
+})
+export class Showcase2v2Component implements OnInit {
+  @Input() content: any;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
