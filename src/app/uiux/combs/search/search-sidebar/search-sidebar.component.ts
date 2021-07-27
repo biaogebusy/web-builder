7634c7f7204b1d3193343aa261ffe34425a2906f
biaogebusy <349255833@qq.com>
2021-07-27 18:10:31 +0800

    feat: treeview

--- src/app/uiux/combs/search/search-sidebar/search-sidebar.component.ts
+++ src/app/uiux/combs/search/search-sidebar/search-sidebar.component.ts
@@ -1,6 +1,86 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { TreeviewItem } from 'ngx-treeview';
 
+const treeView = [
+  {
+    title: '四分法',
+    elements: {
+      text: '四分法',
+      value: 'all',
+      children: [
+        {
+          text: '金融科技',
+          value: 173,
+          children: [
+            {
+              text: '金融基础设施',
+              value: 186,
+            },
+          ],
+        },
+        {
+          text: '金融体系',
+          value: 174,
+          children: [
+            {
+              text: '基础设施金融',
+              value: 197,
+            },
+            {
+              text: '金融公司管理与重整',
+              value: 203,
+              children: [
+                {
+                  text: '公司管理',
+                  value: 429,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    title: '六分法',
+    elements: {
+      text: '四分法',
+      value: 'all',
+      children: [
+        {
+          text: '金融科技',
+          value: 173,
+          children: [
+            {
+              text: '金融基础设施',
+              value: 186,
+            },
+          ],
+        },
+        {
+          text: '金融体系',
+          value: 174,
+          children: [
+            {
+              text: '基础设施金融',
+              value: 197,
+            },
+            {
+              text: '金融公司管理与重整',
+              value: 203,
+              children: [
+                {
+                  text: '公司管理',
+                  value: 429,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  },
+];
 @Component({
   selector: 'app-search-sidebar',
   templateUrl: './search-sidebar.component.html',
@@ -8,48 +88,25 @@ import { TreeviewItem } from 'ngx-treeview';
 })
 export class SearchSidebarComponent implements OnInit {
   @Input() content: any;
-  items = new TreeviewItem({
-    text: 'IT',
-    value: 9,
-    children: [
-      {
-        text: 'Programming',
-        value: 91,
-        children: [
-          {
-            text: 'Frontend',
-            value: 911,
-            children: [
-              { text: 'Angular 1', value: 9111 },
-              { text: 'Angular 2', value: 9112 },
-              { text: 'ReactJS', value: 9113 },
-            ],
-          },
-          {
-            text: 'Backend',
-            value: 912,
-            children: [
-              { text: 'C#', value: 9121 },
-              { text: 'Java', value: 9122 },
-              { text: 'Python', value: 9123, checked: false },
-            ],
-          },
-        ],
-      },
-      {
-        text: 'Networking',
-        value: 92,
-        children: [
-          { text: 'Internet', value: 921 },
-          { text: 'Security', value: 922 },
-        ],
-      },
-    ],
-  });
-
+  treeView: any[];
+  panelOpenState = false;
+  config = {
+    hasAllCheckBox: false,
+    hasFilter: false,
+    hasCollapseExpand: false,
+    decoupleChildFromParent: false,
+    maxHeight: 500,
+  };
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.treeView = treeView.map((item) => {
+      return {
+        title: item.title,
+        tree: [new TreeviewItem(item.elements)],
+      };
+    });
+  }
 
   onSelectedChange(event: any): void {
     console.log(event);
