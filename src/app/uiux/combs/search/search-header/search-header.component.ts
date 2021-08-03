ab848d1e832331d074fb00a87139137db1e8709c
biaogebusy <349255833@qq.com>
2021-08-03 18:15:20 +0800

    quick fix

--- src/app/uiux/combs/search/search-header/search-header.component.ts
+++ src/app/uiux/combs/search/search-header/search-header.component.ts
@@ -6,22 +6,14 @@ import {
   Output,
   ViewChild,
   ElementRef,
-  AfterViewInit,
 } from '@angular/core';
-import { fromEvent, Subscription } from 'rxjs';
-import {
-  debounceTime,
-  distinctUntilChanged,
-  map,
-  startWith,
-} from 'rxjs/operators';
-
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-search-header',
   templateUrl: './search-header.component.html',
   styleUrls: ['./search-header.component.scss'],
 })
-export class SearchHeaderComponent implements OnInit, AfterViewInit {
+export class SearchHeaderComponent implements OnInit {
   @Input() content: any;
   @Input() key: string;
   @Output() searchChange = new EventEmitter();
