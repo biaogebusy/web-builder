9f7987a8384da46885ec2b871d5cdf1c847c05e1
biaogebusy <349255833@qq.com>
2021-09-15 00:08:58 +0800

    feat: share

--- src/app/uiux/combs/list/list/list-thin/list-thin.component.ts
+++ src/app/uiux/combs/list/list/list-thin/list-thin.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  AfterViewInit,
+} from '@angular/core';
 import { ScreenService } from 'src/app/service/screen.service';
-
+declare var window: any;
 @Component({
   selector: 'app-list-thin',
   templateUrl: './list-thin.component.html',
   styleUrls: ['./list-thin.component.scss'],
 })
-export class ListThinComponent implements OnInit {
+export class ListThinComponent implements OnInit, AfterViewInit {
   @Input() content: any;
   @Input() loading: boolean;
   @Input() pager: any;
@@ -16,6 +23,10 @@ export class ListThinComponent implements OnInit {
   constructor(private screenService: ScreenService) {}
 
   ngOnInit(): void {}
+
+  open(): void {
+    window.socialShare('.share-components');
+  }
 
   onPageChange(link: string): void {
     this.pageChange.emit(link);
