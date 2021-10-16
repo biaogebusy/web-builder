268e824e659c0b90a7ace75124d5ab5a0224ae23
biaogebusy <349255833@qq.com>
2021-10-16 23:09:05 +0800

    feat: hero 2v3 support row

--- src/app/uiux/combs/hero/hero2v3/hero2v3.component.ts
+++ src/app/uiux/combs/hero/hero2v3/hero2v3.component.ts
@@ -1,15 +1,25 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ScreenState } from 'src/app/mobx/screen/ScreenState';
+import { NodeService } from 'src/app/service/node.service';
+import { BaseComponent } from 'src/app/uiux/base/base.widget';
 import { AppState } from '../../../../mobx/AppState';
+import { RouteService } from 'src/app/service/route.service';
 
 @Component({
   selector: 'app-hero2v3',
   templateUrl: './hero2v3.component.html',
   styleUrls: ['./hero2v3.component.scss'],
 })
-export class Hero2v3Component implements OnInit {
+export class Hero2v3Component extends BaseComponent implements OnInit {
   @Input() content: any;
-  constructor(public appState: AppState, public screen: ScreenState) {}
+  constructor(
+    public appState: AppState,
+    public screen: ScreenState,
+    public nodeService: NodeService,
+    public routerService: RouteService
+  ) {
+    super(nodeService, routerService);
+  }
 
   ngOnInit(): void {}
 }
