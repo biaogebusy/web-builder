212c389182d41777a3ad7581e57c6beb056caf32
biaogebusy <349255833@qq.com>
2021-10-18 14:34:31 +0800

    feat: menu list expand

--- src/app/uiux/widgets/menu-list/menu-list.component.ts
+++ src/app/uiux/widgets/menu-list/menu-list.component.ts
@@ -7,7 +7,32 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class MenuListComponent implements OnInit {
   @Input() content: any;
+  list: any[];
+  initList: any[];
+  expand = false;
+  row = 12;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initContent(this.expand);
+  }
+
+  initContent(expand: boolean): void {
+    const lists = this.content.elements;
+    this.initList = lists;
+    this.updateList(this.expand);
+  }
+
+  updateList(expand: boolean): void {
+    if (expand) {
+      this.list = this.initList;
+    } else {
+      this.list = this.initList.slice(0, this.row - 1);
+    }
+  }
+
+  onExpand(): void {
+    this.expand = !this.expand;
+    this.updateList(this.expand);
+  }
 }
