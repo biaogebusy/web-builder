f3879a20de9707cc2b1fdce9fab5463a54c88a00
biaogebusy <349255833@qq.com>
2021-12-27 18:38:36 +0800

    feat: forcedialog

--- src/app/core/service/dialog.service.ts
+++ src/app/core/service/dialog.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogComponent } from '@uiux/widgets/dialog/dialog.component';
 import { TextComponent } from '../../uiux/widgets/text/text.component';
 import { AppState } from '@core/mobx/AppState';
+import { DOCUMENT } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
@@ -11,28 +12,54 @@ import { AppState } from '@core/mobx/AppState';
 export class DialogService {
   dialogState$ = new Subject();
   dialogRef: any;
-  constructor(private dialog: MatDialog, private appState: AppState) {}
+  constructor(
+    private dialog: MatDialog,
+    private appState: AppState,
+    @Inject(DOCUMENT) private doc: Document
+  ) {}
 
   closeDialog(): void {
     this.dialogState$.next(false);
   }
 
   forceDialog(config: any): void {
+    const first = this.doc.referrer;
+    const path = this.doc.location.pathname;
+    const params = config.params;
+    if (!config) {
+      return;
+    }
+
+    if (params.first && !first) {
+      if (params.path) {
+        if (path.includes(params.path)) {
+          this.openDialog(config);
+        }
+      } else {
+        this.openDialog(config);
+      }
+      return;
+    } else {
+      if (params.path) {
+        if (path.includes(params.path)) {
+          this.openDialog(config);
+        }
+        return;
+      }
+    }
+
+    this.openDialog(config);
+  }
+
+  openDialog(config: any): void {
     this.dialogRef = this.dialog.open(DialogComponent, {
-      width: config.width,
+      width: config.params.width,
       disableClose: true,
       data: {
         renderInputComponent: TextComponent,
         disableCloseButton: true,
         inputData: {
-          content: {
-            title: {
-              label: config.title,
-              style: 'style-v1',
-            },
-            body: config.content,
-            actions: config.actions,
-          },
+          content: config.text,
         },
       },
     });
