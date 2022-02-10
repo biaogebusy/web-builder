1988d7cc8011de3953ebf38a577097219196e24e
biaogebusy <349255833@qq.com>
2022-02-10 15:47:39 +0800

    update ts file

---
+++ src/app/core/interceptors/serverState-interceptor.service.ts
@@ -0,0 +1,32 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+  HttpResponse,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { makeStateKey, TransferState } from '@angular/platform-browser';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class ServerStateInterceptor implements HttpInterceptor {
+  constructor(private transferState: TransferState) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      tap((event) => {
+        if (
+          event instanceof HttpResponse &&
+          (event.status === 200 || event.status === 202)
+        ) {
+          this.transferState.set(makeStateKey(req.url), event.body);
+        }
+      })
+    );
+  }
+}
