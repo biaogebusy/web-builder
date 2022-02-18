0e49bf81dd8009a29d6db3498546c853fbe504d5
biaogebusy <349255833@qq.com>
2022-02-18 17:03:46 +0800

    add sanziqi

---
+++ src/environments/environment.sanziqi.ts
@@ -0,0 +1,8 @@
+import { IEnvironment } from '../app/core/interface/IEnvironment';
+
+export const environment: IEnvironment = {
+  apiUrl: 'https://sanziqi-api.zhaobg.com',
+  production: true,
+  site: 'sanziqi',
+  port: 4207,
+};
