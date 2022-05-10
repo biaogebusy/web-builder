58fabbb94a2c91a0e836c85fb1871f9616f25fa5
biaogebusy <349255833@qq.com>
2022-05-10 21:38:52 +0800

    feat: map list v1

--- src/app/core/interface/widgets/IMediaObject.ts
+++ src/app/core/interface/widgets/IMediaObject.ts
@@ -1,8 +1,10 @@
 import { IImg } from './IImg';
 import { ILink } from './ILink';
+import { IIcon } from './IIcon';
 export interface IMediaObject {
   css3: boolean;
   img: IImg;
+  icon: IIcon;
   meta?: HTMLElement;
   title?: string;
   subTitle?: string;
