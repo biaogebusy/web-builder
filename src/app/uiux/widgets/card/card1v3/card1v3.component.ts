917790e6c8d1c5767aa3c1db99dfd47462794a4c
biaogebusy <349255833@qq.com>
2022-06-09 13:53:45 +0800

    feat: card 1v3

--- src/app/uiux/widgets/card/card1v3/card1v3.component.ts
+++ src/app/uiux/widgets/card/card1v3/card1v3.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
 import { IMark } from '@core/interface/IAmap';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from '@uiux/widgets/dialog/dialog.component';
+import { TextComponent } from '@uiux/widgets/text/text.component';
 
 @Component({
   selector: 'app-card1v3',
@@ -10,12 +13,36 @@ export class Card1v3Component implements OnInit {
   @Input() content: any;
   @Output() selected = new EventEmitter();
   selectedId: number;
-  constructor() {}
+  constructor(private dialog: MatDialog) {}
 
   ngOnInit(): void {}
 
   onClick(item: any, i: number): void {
     this.selectedId = i;
+    if (item.dialog) {
+      this.openDialog(item);
+      return;
+    }
     this.selected.emit({ item, index: i } as IMark);
+  }
+
+  openDialog(item: any): void {
+    this.dialog.open(DialogComponent, {
+      width: '600px',
+      data: {
+        renderInputComponent: TextComponent,
+        inputData: {
+          content: {
+            type: 'text',
+            spacer: 'none',
+            title: {
+              label: item.title,
+              style: 'style-v4',
+            },
+            body: item.dialog.content,
+          },
+        },
+      },
+    });
   }
 }
