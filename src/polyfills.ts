d0c3631671b7208131996c5a7aeef48ef5874b2e
biaogebusy <349255833@qq.com>
2022-06-29 00:17:17 +0800

    fix: support ie

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -59,6 +59,18 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
 import 'core-js/es/object';
 import 'core-js/es/symbol';
 import 'core-js/es/array';
+import 'core-js/es/function';
+import 'core-js/es/parse-int';
+import 'core-js/es/parse-float';
+import 'core-js/es/number';
+import 'core-js/es/math';
+import 'core-js/es/string';
+import 'core-js/es/date';
+import 'core-js/es/regexp';
+import 'core-js/es/map';
+import 'core-js/es/weak-map';
+import 'core-js/es/set';
+import 'core-js/es/reflect';
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
