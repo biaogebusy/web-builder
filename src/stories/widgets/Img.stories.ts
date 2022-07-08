a1c0bae10515f9c777c9f4f9c40597a51ffde24e
biaogebusy <349255833@qq.com>
2022-07-08 23:01:52 +0800

    update story

--- src/stories/widgets/Img.stories.ts
+++ src/stories/widgets/Img.stories.ts
@@ -1,35 +1,62 @@
-import { storiesOf, moduleMetadata, Story } from '@storybook/angular';
-import { ImgComponent } from '@uiux/widgets/img/img.component';
+import { SafeHtmlPipe } from '@core/pipe/safe-html.pipe';
 import { ShareModule } from '@share/share.module';
+import {
+  moduleMetadata,
+  Meta,
+  Story,
+  componentWrapperDecorator,
+} from '@storybook/angular';
+import { MatIconModule } from '@angular/material/icon';
+import { WidgetsModule } from '@uiux/widgets/widgets.module';
+import { ImgComponent } from '@uiux/widgets/img/img.component';
 import { CORE_CONFIG } from '@core/token/core.config';
 
-storiesOf('Img', module)
-  .addDecorator(
+export default {
+  title: 'Widgets/Img',
+  component: ImgComponent,
+  decorators: [
     moduleMetadata({
-      imports: [ShareModule],
       declarations: [],
+      imports: [MatIconModule, ShareModule, WidgetsModule],
       providers: [
+        SafeHtmlPipe,
         {
           provide: CORE_CONFIG,
           useValue: {},
         },
       ],
-    })
-  )
-  .add('Img', () => ({
-    component: ImgComponent,
-    argTypes: {
-      content: {
-        hostClasses: 'bg-center',
-        src: '/assets/images/banner-01.jpeg',
-        alt: 'page title',
-      },
-    },
-    props: {
-      content: {
-        hostClasses: 'bg-center',
-        src: '/assets/images/banner-01.jpeg',
-        alt: 'page title',
-      },
-    },
-  }));
+    }),
+    componentWrapperDecorator(
+      (story) => `<div style="height:300px">${story}</div>`
+    ),
+  ],
+} as Meta;
+
+const Template: Story<ImgComponent> = (args) => ({
+  component: ImgComponent,
+  props: {
+    ...args,
+  },
+});
+
+export const Default = Template.bind({});
+
+Default.args = {
+  content: {
+    classes: 'object-fit',
+    src: '/assets/images/cases/porto2.jpg',
+    alt: 'alt',
+  },
+};
+
+export const ImgWithLink = Template.bind({});
+
+ImgWithLink.args = {
+  content: {
+    classes: 'object-fit',
+    src: '/assets/images/cases/porto2.jpg',
+    alt: 'alt',
+    href: '#',
+    target: '_blank',
+  },
+};
