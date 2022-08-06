926284e9bb7922b42328b08cbc17c05107e73a80
biaogebusy <349255833@qq.com>
2022-08-06 15:11:51 +0800

    update carousel 1v1 sample

--- src/stories/components/carousel/carousel1v1.stories.ts
+++ src/stories/components/carousel/carousel1v1.stories.ts
@@ -200,3 +200,115 @@ Default.args = {
     },
   },
 };
+
+export const TextHero = Template.bind({});
+TextHero.args = {
+  content: {
+    spacer: 'none',
+    disableContainer: true,
+    sliders: {
+      params: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        navigation: false,
+        pagination: false,
+      },
+      classes: 'p-bottom',
+      elements: [
+        {
+          type: 'text-hero',
+          theme: 'text-light',
+          params: {
+            height: '750px',
+          },
+          text: {
+            title: {
+              label: '私募基金业务',
+              style: 'style-v4',
+              classes: 'mat-display-2 bold',
+            },
+            classes: 'y-center',
+            style: [],
+            bg: {
+              classes: 'bg-shadow overlay overlay-20',
+              img: {
+                src: '/assets/images/hero/1-3.jpg',
+                hostClasses: '',
+              },
+            },
+            body: '<p>精通私募股权投资和风险投资所涉诸多法律事务，并在该领域拥有广泛的客户群体</p>\r\n',
+            actionsAlign: 'start center',
+            actions: [
+              {
+                href: '',
+                label: '了解更多',
+              },
+            ],
+          },
+        },
+        {
+          type: 'text-hero',
+          theme: 'text-light',
+          params: {
+            height: '750px',
+          },
+          text: {
+            title: {
+              label: '风险投资、收购兼并',
+              style: 'style-v4',
+              classes: 'mat-display-2 bold',
+            },
+            classes: 'y-center',
+            style: [],
+            bg: {
+              classes: 'bg-shadow overlay overlay-',
+              img: {
+                src: '/assets/images/hero/1-6.jpg',
+                hostClasses: '',
+              },
+            },
+            body: '<p>在并购与重组领域拥有丰富的经验，多年来处于市场领先地位并得到广泛认可，曾代表众多国内和跨境并购交易</p>\r\n',
+            actionsAlign: 'start center',
+            actions: [
+              {
+                href: '',
+                label: '了解更多',
+              },
+            ],
+          },
+        },
+        {
+          type: 'text-hero',
+          theme: 'text-light',
+          params: {
+            height: '750px',
+          },
+          text: {
+            title: {
+              label: '新架构介绍，使用指南',
+              style: 'style-v4',
+              classes: 'mat-display-2 bold',
+            },
+            classes: 'y-center',
+            style: [],
+            bg: {
+              classes: 'bg-shadow overlay overlay-',
+              img: {
+                src: '/assets/images/hero/182.jpg',
+                hostClasses: '',
+              },
+            },
+            body: '<p>全新的用户前台框架，体验更高效和流畅，为工作带来更多的效率</p>\r\n',
+            actionsAlign: 'start center',
+            actions: [
+              {
+                href: '',
+                label: '了解更多',
+              },
+            ],
+          },
+        },
+      ],
+    },
+  },
+};
