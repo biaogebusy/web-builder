144a6cd6d119e91ad5df2da89641691d54f5add5
biaogebusy <349255833@qq.com>
2022-08-23 21:48:36 +0800

    quick fix

--- src/app/core/service/notify.service.ts
+++ src/app/core/service/notify.service.ts
@@ -18,51 +18,55 @@ export class NotifyService {
   ) {}
 
   watchNotify(): void {
-    const apis = this.coreConfig.notify.api;
-    const source = interval(
-      this.coreConfig.notify.params.interval || 2 * 60 * 1000
-    );
-    // https://ngx-toastr.vercel.app/
-    source.subscribe(() => {
-      this.getWatchList()
-        .pipe(
-          catchError((error: any) => {
-            console.log(error);
-            return of(null);
-          })
-        )
-        .subscribe((res) => {
-          if (!isEmpty(res)) {
-            Object.keys(res).forEach((index) => {
-              if (res[index].rows.length) {
-                const lists = res[index].rows;
-                console.log(res[index].rows);
-                lists.forEach((item: any) => {
-                  const config = apis[index];
-                  this.toastr
-                    .show(
-                      item.label,
-                      config.title,
-                      config.options,
-                      config.type || 'success'
-                    )
-                    .onHidden.subscribe(() => {
-                      console.log('closed!');
-                    });
-                });
-              }
-            });
-          }
-        });
-    });
+    const apis = this.coreConfig?.notify?.api;
+    if (apis) {
+      const source = interval(
+        this.coreConfig?.notify?.params.interval || 2 * 60 * 1000
+      );
+      // https://ngx-toastr.vercel.app/
+      source.subscribe(() => {
+        this.getWatchList()
+          .pipe(
+            catchError((error: any) => {
+              console.log(error);
+              return of(null);
+            })
+          )
+          .subscribe((res) => {
+            if (!isEmpty(res)) {
+              Object.keys(res).forEach((index: any) => {
+                if (res[index].rows.length) {
+                  const lists = res[index].rows;
+                  console.log(res[index].rows);
+                  lists.forEach((item: any) => {
+                    const config = apis[index];
+                    this.toastr
+                      .show(
+                        item.label,
+                        config.title,
+                        config.options,
+                        config.type || 'success'
+                      )
+                      .onHidden.subscribe(() => {
+                        console.log('closed!');
+                      });
+                  });
+                }
+              });
+            }
+          });
+      });
+    }
   }
 
   getWatchList(): Observable<any> {
     const obj: any = {};
-    const apiList = this.coreConfig.notify.api;
-    apiList.forEach((list: any, index: number) => {
-      obj[index] = this.nodeService.getNodes(list.get, '');
-    });
+    const apiList = this.coreConfig?.notify?.api;
+    if (apiList && apiList?.length > 0) {
+      apiList.forEach((list: any, index: number) => {
+        obj[index] = this.nodeService.getNodes(list.get, '');
+      });
+    }
 
     return forkJoin(obj);
   }
