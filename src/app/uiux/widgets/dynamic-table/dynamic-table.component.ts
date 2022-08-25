fbdc2e82a02b0b3ced9dd07b8c4561f7e7bbe5f7
biaogebusy <349255833@qq.com>
2022-08-25 18:11:50 +0800

    feat: table expand

--- src/app/uiux/widgets/dynamic-table/dynamic-table.component.ts
+++ src/app/uiux/widgets/dynamic-table/dynamic-table.component.ts
@@ -10,24 +10,44 @@ import { DialogComponent } from '../dialog/dialog.component';
 import type { IDynamicTable } from '../IWidgets';
 import { TextComponent } from '../text/text.component';
 import { RouteService } from '@core/service/route.service';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-dynamic-table',
   templateUrl: './dynamic-table.component.html',
   styleUrls: ['./dynamic-table.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+      ),
+    ]),
+  ],
 })
 export class DynamicTableComponent implements OnInit {
   @Input() content: IDynamicTable;
   @Input() form: FormGroup;
 
   displayedColumns: string[];
+  columnsToDisplayWithExpand: string[];
+  expandedElement: null;
 
   constructor(private dialog: MatDialog, private routService: RouteService) {}
 
   ngOnInit(): void {
     if (this.content.elements) {
       this.displayedColumns = this.content.header.map((item: any) => item.key);
+      this.columnsToDisplayWithExpand = [...this.displayedColumns, 'expand'];
     }
   }
 
