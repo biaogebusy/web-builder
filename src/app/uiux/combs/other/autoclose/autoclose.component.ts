d0a1d302f0fcf8aca107a13d2b94cab7290ef3d0
biaogebusy <349255833@qq.com>
2022-09-02 14:50:59 +0800

    feat: auto close

---
+++ src/app/uiux/combs/other/autoclose/autoclose.component.ts
@@ -0,0 +1,21 @@
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+import { CORE_CONFIG } from '@core/token/core.config';
+import { ICoreConfig } from '@core/mobx/IAppConfig';
+
+@Component({
+  selector: 'app-autoclose',
+  templateUrl: './autoclose.component.html',
+  styleUrls: ['./autoclose.component.scss'],
+})
+export class AutocloseComponent implements OnInit {
+  @Input() content: any;
+  constructor(@Inject(CORE_CONFIG) private coreConfig: ICoreConfig) {}
+
+  ngOnInit(): void {
+    const source = interval(2 * 1000);
+    source.subscribe(() => {
+      window.parent.postMessage('closeDialog', '*');
+    });
+  }
+}
