0d203c0b75e21b0bdcf7ab87ecd5dd8d757a9672
biaogebusy <349255833@qq.com>
2022-09-14 23:29:03 +0800

    update package

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,10 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-postcss",
   ],
-  "framework": "@storybook/angular"
-}
+  framework: "@storybook/angular",
+};
