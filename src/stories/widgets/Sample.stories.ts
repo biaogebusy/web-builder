57de8e7c4e469cd551cae9083b257f313953c10c
biaogebusy <349255833@qq.com>
2022-09-22 17:28:16 +0800

    update storybook

--- src/stories/widgets/Sample.stories.ts
+++ src/stories/widgets/Sample.stories.ts
@@ -15,16 +15,23 @@ import { API_URL, apiUrlFactory } from '@core/token/token-providers';
 import { APP_INITIALIZER, Inject } from '@angular/core';
 import { AppState } from '@core/mobx/AppState';
 import { initConfig } from 'src/app/app.module';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxWebstorageModule } from 'ngx-webstorage';
+import { TextComponent } from '@uiux/widgets/text/text.component';
+import { TitleComponent } from '@uiux/widgets/title/title.component';
 
 export default {
   title: '基础/示例',
-  component: BgComponent,
+  component: TextComponent,
+  subcomponents: { TitleComponent },
   decorators: [
     moduleMetadata({
       declarations: [],
       imports: [
         ShareModule,
         WidgetsModule,
+        HttpClientModule,
+        NgxWebstorageModule.forRoot(),
         RouterTestingModule,
         BrowserAnimationsModule,
       ],
@@ -55,14 +62,19 @@ export default {
   parameters: {
     docs: {
       description: {
-        component: `描述`,
+        component: `对组件的介绍描述`,
       },
+    },
+  },
+  argTypes: {
+    content: {
+      description: '字段描述',
     },
   },
 } as Meta;
 
-const Template: Story<BgComponent> = (args) => ({
-  component: BgComponent,
+const Template: Story<TextComponent> = (args) => ({
+  component: TextComponent,
   props: {
     ...args,
   },
@@ -71,6 +83,12 @@ export const Default = Template.bind({});
 
 Default.args = {
   content: {
-    classes: 'bg-fill-width bg-primary',
+    spacer: 'sm',
+    title: {
+      label:
+        '开源项目使用 Github actions 自动化测试部署 Angular 应用到 ECS 服务器',
+      style: 'style-v1',
+    },
+    body: '<p>Github actions 从2019年就免费开放给个人开源项目使用，对于自动化开放测试部署，开发者一定非常的熟悉，如果把中间这项流程做好，不仅节省了大量的人力也大大加快了开发效率，在配置完善的情况下可以提高代码质量。</p><p>查了些文档文章，Github Actions有几个重要的信息如下：</p><ul class="list-done"><li>通过 Docker 隔离</li><li>每个 workflow 独享1核虚拟CPU, 3.75GB内存，包括网络权限和100GB 磁盘</li><li>在 yml 配置文件中可以使用上下文环境变量，比如分支或者不公开变量</li><li>每个workflow 排队和执行时间最多 58 分钟，最多可以包含100个action，每个仓库同一时刻只能运行两个 workflow</li></ul>',
   },
 };
