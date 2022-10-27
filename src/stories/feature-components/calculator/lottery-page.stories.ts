632387d237b9cd7e98e3df38ad3e9eb5bac6f3ff
biaogebusy <349255833@qq.com>
2022-10-27 15:07:37 +0800

    update lottery storybook pay

--- src/stories/feature-components/calculator/lottery-page.stories.ts
+++ src/stories/feature-components/calculator/lottery-page.stories.ts
@@ -4,9 +4,10 @@ import {
   componentWrapperDecorator,
 } from '@storybook/angular';
 import { Story } from '@storybook/angular/types-6-0';
+import { screen, userEvent } from '@storybook/testing-library';
 import { BlockComponent } from '@uiux/combs/block/block/block.component';
 import { BlockModule } from '@uiux/combs/block/block.module';
-import { StorysModule } from '@core/storys.module';
+import { sleep, StorysModule } from '@core/storys.module';
 import { BrandingModule } from '@core/branding/branding.module';
 
 export default {
@@ -42,4 +43,44 @@ const Template: Story = (args) => ({
 });
 export const Page = Template.bind({});
 // Raname Story
-Page.storyName = '预览';
+Page.storyName = '预算';
+Page.play = async () => {
+  await sleep(3000);
+  const MaxTotal: any = document.getElementById('maxTotal');
+  await userEvent.type(MaxTotal, '300', {
+    delay: 200,
+  });
+
+  await sleep(1000);
+  const MaxPer: any = document.getElementById('maxPer');
+  await userEvent.type(MaxPer, '10', {
+    delay: 200,
+  });
+
+  await sleep(1000);
+  const MinTotal: any = document.getElementById('minTotal');
+  await userEvent.type(MinTotal, '500', {
+    delay: 200,
+  });
+
+  await sleep(1000);
+  const MinPer: any = document.getElementById('minPer');
+  await userEvent.type(MinPer, '3', {
+    delay: 200,
+  });
+
+  const Form = document.getElementsByTagName('app-formly')[0];
+  await userEvent.click(Form);
+
+  await sleep(2000);
+  await userEvent.clear(MinPer);
+
+  await sleep(1000);
+  await userEvent.type(MinPer, '0.8', {
+    delay: 200,
+  });
+
+  await sleep(2000);
+  const Way = screen.getByRole('switch');
+  await userEvent.click(Way);
+};
