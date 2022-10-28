3013f53c8ec0f8402d264f2fda747176b2f147f4
biaogebusy <349255833@qq.com>
2022-10-28 17:10:26 +0800

    update product storybook

---
+++ src/stories/sample/node/product-v1.stories.ts
@@ -0,0 +1,45 @@
+import {
+  moduleMetadata,
+  Meta,
+  componentWrapperDecorator,
+} from '@storybook/angular';
+import { Story } from '@storybook/angular/types-6-0';
+import { BlockComponent } from '@uiux/combs/block/block/block.component';
+import { BlockModule } from '@uiux/combs/block/block.module';
+import { StorysModule } from '@core/storys.module';
+import { BrandingModule } from '@core/branding/branding.module';
+
+export default {
+  title: '示例页面/产品介绍/v1',
+  id: 'product-v1',
+  component: BlockComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [],
+      imports: [BlockModule, BrandingModule, StorysModule.forRoot()],
+    }),
+    componentWrapperDecorator(
+      (story) => `
+    <app-header></app-header>
+    ${story}
+    <app-footer></app-footer>
+    `
+    ),
+  ],
+  parameters: {
+    docs: {
+      description: {
+        component: ``,
+      },
+    },
+  },
+} as Meta;
+
+const Template: Story = (args) => ({
+  props: {
+    ...args,
+  },
+});
+export const Page = Template.bind({});
+// Raname Story
+Page.storyName = '预览';
