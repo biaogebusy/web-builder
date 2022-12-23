9933e20717e2c7daaa5fb58d4a319bc36301d699
biaogebusy <349255833@qq.com>
2022-12-23 22:56:04 +0800

    feat: manage sidebar

--- src/app/core/branding/manage-sidebar/manage-sidebar.component.ts
+++ src/app/core/branding/manage-sidebar/manage-sidebar.component.ts
@@ -1,8 +1,6 @@
 import { Component, Inject, Input, OnInit } from '@angular/core';
-import { CORE_CONFIG, BRANDING, USER } from '@core/token/token-providers';
+import { CORE_CONFIG, USER } from '@core/token/token-providers';
 import { ICoreConfig } from '@core/interface/IAppConfig';
-import { IBranding } from '@core/interface/IBranding';
-import { Observable } from 'rxjs';
 import { IUser } from '@core/interface/IUser';
 import { UserService } from '@core/service/user.service';
 
@@ -15,7 +13,6 @@ export class ManageSidebarComponent implements OnInit {
   @Input() content: any;
   constructor(
     @Inject(CORE_CONFIG) public coreConfig: ICoreConfig,
-    @Inject(BRANDING) public branding$: Observable<IBranding>,
     @Inject(USER) public user: IUser,
     public userSerivice: UserService
   ) {}
