0b9dddbf1adfc0c147f3690f3a62ce211051b6b4
biaogebusy <349255833@qq.com>
2022-12-27 12:39:53 +0800

    refact: update

--- src/app/modules/manage/manage.module.ts
+++ src/app/modules/manage/manage.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { ShareModule } from '@share/share.module';
 import { ManageRoutingModule } from './manage-routing.module';
 import { ManageMediaComponent } from './manage-media/manage-media.component';
-import { UiuxModule } from '@uiux/uiux.module';
+import { WidgetsModule } from '@uiux/widgets/widgets.module';
 import { ManageBlockComponent } from './manage-block/manage-block.component';
 const components = [ManageMediaComponent, ManageBlockComponent];
 
 @NgModule({
   declarations: [...components],
-  imports: [ShareModule, UiuxModule, ManageRoutingModule],
+  imports: [ShareModule, WidgetsModule, ManageRoutingModule],
 })
 export class ManageModule {}
--- src/app/uiux/uiux.module.ts
+++ src/app/uiux/uiux.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { WidgetsModule } from './widgets/widgets.module';
 import { CombsModule } from './combs/combs.module';
 
-const modules = [WidgetsModule, CombsModule];
+const modules = [CombsModule];
 
 @NgModule({
   declarations: [],
