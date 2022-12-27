e4e23d89941918315eb2f9c7634a51d1c65e8b20
biaogebusy <349255833@qq.com>
2022-12-27 12:40:46 +0800

    remove no need package

--- src/app/uiux/uiux.module.ts
+++ src/app/uiux/uiux.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { WidgetsModule } from './widgets/widgets.module';
 import { CombsModule } from './combs/combs.module';
 
 const modules = [CombsModule];
