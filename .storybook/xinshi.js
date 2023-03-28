90f785517c14aafae4340f192e902819f240b0ff
biaogebusy <349255833@qq.com>
2023-03-28 16:05:33 +0800

    feat: storybook support theme switch

---
+++ .storybook/xinshi.js
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "light",
+  brandTitle: "信使",
+  brandUrl: "/",
+  brandImage: "/assets/images/logo/logo-blue.png",
+  brandTarget: "_self",
+});
