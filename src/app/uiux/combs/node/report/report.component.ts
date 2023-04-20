bb43cb2a40b5fc2e5b387124846b0957d0e9812d
biaogebusy <349255833@qq.com>
2023-04-20 23:09:32 +0800

    feat: report

--- src/app/uiux/combs/node/report/report.component.ts
+++ src/app/uiux/combs/node/report/report.component.ts
@@ -50,61 +50,63 @@ export class ReportComponent extends BaseComponent implements OnInit {
     const params = this.getApiParams(options);
     const api = this.content.params.api;
     this.nodeService.search(api, params).subscribe(({ chart, table }) => {
-      if (isArray(chart) && isArray(table)) {
-        this.box = [
-          {
-            data: {
-              toggle: [...this.content.toggle],
-            },
-            content: {
-              type: 'chart',
-              tooltip: {
-                trigger: 'item',
-              },
-              legend: {
-                orient: 'vertical',
-                left: 'left',
-              },
-              dataset: [
-                {
-                  source: chart,
-                },
-                ...this.content.customDataset,
-              ],
-              xAxis: {
-                type: 'category',
-                axisLabel: {
-                  interval: 0,
-                  rotate: 30,
-                },
-              },
-              yAxis: {
-                type: 'value',
-              },
-              series: [
-                {
-                  type: 'bar',
-                  radius: '50%',
-                  center: ['50%', '50%'],
-                  label: {
-                    position: 'top',
-                    show: true,
-                  },
-                  datasetIndex: this.content.datasetIndex || 0,
-                },
-              ],
-              ...this.content.options,
-            },
+      if (isArray(chart)) {
+        this.box.push({
+          data: {
+            toggle: [...this.content.toggle],
           },
-          {
-            content: {
-              type: 'dynamic-table',
-              header: [...this.content.tableHeader],
-              elements: table,
+          content: {
+            type: 'chart',
+            tooltip: {
+              trigger: 'item',
             },
+            legend: {
+              orient: 'vertical',
+              left: 'left',
+            },
+            dataset: [
+              {
+                source: chart,
+              },
+              ...this.content.customDataset,
+            ],
+            xAxis: {
+              type: 'category',
+              axisLabel: {
+                interval: 0,
+                rotate: 30,
+              },
+            },
+            yAxis: {
+              type: 'value',
+            },
+            series: [
+              {
+                type: 'bar',
+                radius: '50%',
+                center: ['50%', '50%'],
+                label: {
+                  position: 'top',
+                  show: true,
+                },
+                datasetIndex: this.content.datasetIndex || 0,
+              },
+            ],
+            ...this.content.options,
           },
-        ];
-      } else {
+        });
+      }
+      if (isArray(table)) {
+        this.box.push({
+          content: {
+            type: 'dynamic-table',
+            header: [...this.content.tableHeader],
+            elements: table,
+          },
+        });
+      }
+
+      if (!isArray(chart) || !isArray(table)) {
         this.box = [
           {
             content: {
