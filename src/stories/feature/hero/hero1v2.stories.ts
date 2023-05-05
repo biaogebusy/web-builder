7a92b1b45c7ebe647e52d1b17f0fcd61b6a3cae3
biaogebusy <349255833@qq.com>
2023-05-05 12:08:00 +0800

    update medical

--- src/stories/feature/hero/hero1v2.stories.ts
+++ src/stories/feature/hero/hero1v2.stories.ts
@@ -44,6 +44,8 @@ Default.args = {
       elements: [
         {
           title: '使用 DEVTOOLS 对 ANGULAR 前端应用性能分析优化',
+          subTitle:
+            '使用 lighthouse 评分 以南宁IT派[www.nnitpai.com]为例记录分析优化过程，使用 Devtools lighthouse 对首页进行综合的评分',
           img: {
             src: '/assets/images/hero/hero-1-v2-4.jpeg',
             alt: '',
@@ -57,6 +59,8 @@ Default.args = {
         },
         {
           title: '使用 TAKEUNTIL 操作符管理 ANGULAR 组件的订阅',
+          subTitle:
+            '在 Rxjs 中，可以使用 takeUntil 来控制另外一个 Observable 对象数据的产生。',
           img: {
             src: '/assets/images/hero/hero-1-v2-3.jpeg',
             alt: '',
@@ -70,6 +74,8 @@ Default.args = {
         },
         {
           title: '你应该了解的 ANGULAR 最佳实践',
+          subTitle:
+            '遵循最佳实践可以让你的 Angular 应用保持性能优越，使团队的代码风格一致，以下代码摘自南宁IT派官网项目。',
           img: {
             src: '/assets/images/hero/hero-1-v2-2.jpeg',
             alt: '',
@@ -83,6 +89,8 @@ Default.args = {
         },
         {
           title: 'ANGULAR 5 RXJS 5.5.2 多个 HTTP 并行 FORKJOIN 请求',
+          subTitle:
+            '在特定情况下，有些接口无法提供一次性的请求达到目的，需要并行的多次请求，当所有请求都完成时，才进行下一步的逻辑。',
           img: {
             src: '/assets/images/hero/hero-1-v2-1.jpeg',
             alt: '',
