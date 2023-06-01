8490ec588600a170ada0e99428c0ae17850b8614
biaogebusy <349255833@qq.com>
2023-06-01 22:18:30 +0800

    config assets alian

--- src/stories/sample/ManageSidebar.stories.ts
+++ src/stories/sample/ManageSidebar.stories.ts
@@ -9,7 +9,7 @@ import { BlockModule } from '@uiux/combs/block/block.module';
 import { StorysModule } from '@core/module/storys.module';
 import { BrandingModule } from '@core/branding/branding.module';
 import { BRANDING } from '@core/token/token-providers';
-import brandingSidebar from '../../assets/app/core/branding-sidebar.json';
+import brandingSidebar from '@assets/app/core/branding-sidebar.json';
 import { of } from 'rxjs';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { NodeModule } from '@uiux/combs/node/node.module';
