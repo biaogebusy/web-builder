943e6b5374e69be07094dc0855cc04c3909b7883
biaogebusy <349255833@qq.com>
2023-06-28 18:16:29 +0800

    remove environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 import { IEnvironment } from '../app/core/interface/IEnvironment';
 
 export const environment: IEnvironment = {
-  apiUrl: 'https://xhb.zhaobg.com',
+  apiUrl: 'http://localhost:4200',
   production: true,
   site: 'dist',
   port: 4209,
   cache: true,
   ssr: false,
-  drupalProxy: true,
+  drupalProxy: false,
 };
