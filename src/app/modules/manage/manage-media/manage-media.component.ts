9e08fec65ecf748096cfa08e4b15b0eaad9fadf8
biaogebusy <349255833@qq.com>
2023-07-07 14:49:35 +0800

    feat: drupal manage files

--- src/app/modules/manage/manage-media/manage-media.component.ts
+++ src/app/modules/manage/manage-media/manage-media.component.ts
@@ -4,8 +4,13 @@ import {
   ChangeDetectionStrategy,
   ChangeDetectorRef,
 } from '@angular/core';
+import { IPaginationLinks } from '@core/interface/widgets/IPaginationLinks';
 import { ManageService } from '@core/service/manage.service';
+import { NodeService } from '@core/service/node.service';
+import { ScreenService } from '@core/service/screen.service';
 import { UtilitiesService } from '@core/service/utilities.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-manage-media',
@@ -15,10 +20,14 @@ import { UtilitiesService } from '@core/service/utilities.service';
 })
 export class ManageMediaComponent implements OnInit {
   content: any;
+  links: IPaginationLinks;
+  destory$: Subject<boolean> = new Subject<boolean>();
   constructor(
     private manageService: ManageService,
     private cd: ChangeDetectorRef,
-    private utli: UtilitiesService
+    private utli: UtilitiesService,
+    private screenService: ScreenService,
+    private nodeService: NodeService
   ) {}
 
   ngOnInit(): void {
@@ -26,52 +35,71 @@ export class ManageMediaComponent implements OnInit {
   }
 
   getFiles(): void {
-    this.manageService.getFiles().subscribe((files) => {
-      console.log(files);
-      const iconPath = '/assets/icons';
-      this.content = files.data.map((item: any) => {
-        const attr = item.attributes;
-        const type = this.utli.getFileType(attr.uri.url);
-        if (type === 'picture') {
-          return {
-            type: 'feature-box',
-            width: '20',
-            fullIcon: 'fullscreen',
-            ratios: 'media-4-3',
-            hoverIcon: true,
-            img: {
-              classes: 'object-fit',
-              src: attr.uri.url,
-              alt: attr.filename,
-            },
-          };
-        } else {
-          return {
-            type: 'feature-box',
-            width: '20',
-            fullIcon: 'fullscreen',
-            ratios: 'media-4-3',
-            hoverIcon: true,
-            openIcon: 'file_download',
-            link: attr.uri.url,
-            img: {
-              classes: 'object-fill p-x-lg p-y-lg',
-              src:
-                type === 'pdf'
-                  ? `${iconPath}/file-pdf.svg`
-                  : type === 'excel'
-                  ? `${iconPath}/file-excel.svg`
-                  : `${iconPath}/file-word.svg`,
-              alt: attr.filename,
-            },
-          };
-        }
+    this.manageService
+      .getFiles()
+      .pipe(takeUntil(this.destory$))
+      .subscribe((res) => {
+        console.log(res);
+        this.updateList(res);
       });
-      this.cd.detectChanges();
+  }
+
+  updateList(res: any): void {
+    const { data, links } = res;
+    const iconPath = '/assets/icons';
+    this.content = data.map((item: any) => {
+      const attr = item.attributes;
+      const type = this.utli.getFileType(attr.uri.url);
+      const widget = {
+        id: item.id,
+        type: 'feature-box',
+        width: '20',
+        fullIcon: 'fullscreen',
+        ratios: 'media-4-3',
+        hoverIcon: true,
+      };
+      if (type === 'picture') {
+        return {
+          ...widget,
+          img: {
+            classes: 'object-fit',
+            src: attr.uri.url,
+            alt: attr.filename,
+          },
+        };
+      } else {
+        return {
+          ...widget,
+          openIcon: 'file_download',
+          link: attr.uri.url,
+          img: {
+            classes: 'object-fill p-x-lg p-y-lg',
+            src:
+              type === 'pdf'
+                ? `${iconPath}/file-pdf.svg`
+                : type === 'excel'
+                ? `${iconPath}/file-excel.svg`
+                : `${iconPath}/file-word.svg`,
+            alt: attr.filename,
+          },
+        };
+      }
     });
+    this.links = links;
+    this.cd.detectChanges();
+  }
+
+  onPageChange(link: string): void {
+    this.screenService.gotoTop();
+    this.nodeService
+      .getNodeByLink(link)
+      .pipe(takeUntil(this.destory$))
+      .subscribe((res) => {
+        this.updateList(res);
+      });
   }
 
   trackByFn(index: number, item: any): number {
-    return index;
+    return item.id;
   }
 }
