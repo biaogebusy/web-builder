729d5a8ad8ee4a896f56e9b66ff7d2b73fb39151
biaogebusy <349255833@qq.com>
2023-09-17 22:52:42 +0800

    feat: builder style

--- src/app/core/service/content.service.ts
+++ src/app/core/service/content.service.ts
@@ -2,7 +2,7 @@ import { DOCUMENT } from '@angular/common';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import type { ICoreConfig, IPage } from '@core/interface/IAppConfig';
-import { API_URL } from '@core/token/token-providers';
+import { API_URL, CORE_CONFIG } from '@core/token/token-providers';
 import { environment } from 'src/environments/environment';
 import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
@@ -28,7 +28,8 @@ export class ContentService {
     private screenState: ScreenState,
     private apiService: ApiService,
     @Inject(API_URL) private apiUrl: string,
-    @Inject(DOCUMENT) private document: Document
+    @Inject(DOCUMENT) private document: Document,
+    @Inject(CORE_CONFIG) private coreConfig: ICoreConfig
   ) {}
 
   updatePage(pageValue: IPage): void {
@@ -60,10 +61,11 @@ export class ContentService {
     if (environment.production) {
       const landingPath = '/api/v1/landingPage?content=';
       const pageUrlParams = `${this.apiUrl}${landingPath}${pageUrl}`;
-
+      console.log(this.coreConfig);
       return this.http.get<any>(pageUrlParams).pipe(
         tap((page) => {
           this.updatePage(page);
+          this.logContent(pageUrl);
         }),
         catchError(() => {
           return this.http.get<any>(`${this.apiUrl}${landingPath}404`);
@@ -80,6 +82,13 @@ export class ContentService {
       } else {
         return this.http.get<any>(`${this.apiUrl}/assets/app/404.json`);
       }
+    }
+  }
+
+  logContent(url: string): void {
+    if (this.coreConfig?.log?.content?.enabel) {
+      const { api } = this.coreConfig.log.content;
+      this.http.get(`${api}?location=${url}`).subscribe();
     }
   }
 
