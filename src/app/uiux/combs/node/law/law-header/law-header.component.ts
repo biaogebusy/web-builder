58d0058a04e774bf51e098e6df0f9245698bef46
biaogebusy <349255833@qq.com>
2023-12-02 14:07:29 +0800

    feat: law header

--- src/app/uiux/combs/node/law/law-header/law-header.component.ts
+++ src/app/uiux/combs/node/law/law-header/law-header.component.ts
@@ -26,7 +26,10 @@ export class LawHeaderComponent implements OnInit, AfterViewInit {
     private screenService: ScreenService,
     private cd: ChangeDetectorRef
   ) {
-    if (this.screenService.isPlatformBrowser() && environment.port === 4208) {
+    if (
+      true ||
+      (this.screenService.isPlatformBrowser() && environment.port === 4208)
+    ) {
       this.loadFonts().then(() => {
         console.log('Load font done!');
         this.disabled = false;
@@ -48,127 +51,147 @@ export class LawHeaderComponent implements OnInit, AfterViewInit {
     await import('./fonts/fangsong_GB2312-normal.js');
   }
 
-  async onSavePdf(): Promise<void> {
-    const pdf = this.content.pdf;
-    if (!pdf) {
-      return;
-    }
-    if (this.screenService.isPlatformServer()) {
-      return;
-    }
-    this.saveDocLoading = true;
+  async buildPdf(): Promise<any> {
+    return new Promise((resolve) => {
+      const pdf = this.content.pdf;
+      if (!pdf) {
+        return;
+      }
+      if (this.screenService.isPlatformServer()) {
+        return;
+      }
+      this.saveDocLoading = true;
 
-    // example: http://raw.githack.com/MrRio/jsPDF/master/index.html
-    const doc = new jsPDF({
-      orientation: 'portrait',
-      unit: 'px',
-    });
-    doc.setProperties({
-      title: this.content.pdf.fileName || this.content.title,
-    });
-    doc.setFont('fangsong_GB2312');
-    doc.setFontSize(12);
-    autoTable(doc, {
-      startY: 33,
-      tableLineWidth: 0,
-      styles: {
-        font: 'simhei',
-        fontStyle: 'bold',
-        fontSize: 1,
-        fillColor: [255, 255, 255],
-        textColor: [0, 0, 0],
-        valign: 'middle',
-        halign: 'center',
-        lineWidth: 0,
-        cellWidth: 'auto',
-        overflow: 'linebreak',
-      },
-      theme: 'grid',
-      margin: {
-        top: 33,
-        right: 33,
-        bottom: 33,
-        left: 50,
-      },
-      body: pdf.table.header,
-      didDrawCell: (data: any) => {
-        // logo
-        if (
-          data.row.index === 0 &&
-          data.column.index === 0 &&
-          data.cell.section === 'body'
-        ) {
-          const textPos = data.cell.getTextPos();
-          doc.addImage(pdf.logo.data, textPos.x - 53, textPos.y - 8, 107, 16);
-        }
-      },
-    });
-    let finalY = (doc as any).lastAutoTable.finalY;
-
-    autoTable(doc, {
-      startY: finalY,
-      styles: {
-        fillColor: [255, 255, 255],
-        fontSize: 12,
-        font: 'fangsong_GB2312',
-        textColor: [0, 0, 0],
-        valign: 'middle',
-        lineWidth: 0.2,
-        lineColor: 5,
-      },
-      tableLineWidth: 0.2,
-      tableLineColor: 5,
-      margin: {
-        top: 33,
-        right: 33,
-        bottom: 33,
-        left: 50,
-      },
-      theme: 'grid',
-      didDrawCell: (data: any) => {
-        // 签名图片
-        if (
-          data.row.index === pdf.sign.row &&
-          data.column.index === pdf.sign.column &&
-          data.cell.section === 'body'
-        ) {
-          console.log(data);
-          const dim = data.cell.height - data.cell.padding('vertical');
-          const textPos = data.cell.getTextPos();
-          doc.addImage(pdf.sign.data, textPos.x, textPos.y - 7, 47, 14);
-        }
-      },
-      body: pdf.table.body,
-    });
-
-    finalY = (doc as any).lastAutoTable.finalY;
-    const now = Date.now();
-    const time = formatDate(new Date(), 'yyyy年MM月dd日 HH:mm', 'zh-Hans');
-    doc.text(`打印日期：${time}`, 50, finalY + 15);
-
-    // page number
-    const internal: any = doc.internal;
-    const pages = internal.getNumberOfPages();
-    const pageWidth = doc.internal.pageSize.width;
-    const pageHeight = doc.internal.pageSize.height;
-    doc.setFontSize(12);
-
-    for (let j = 1; j < pages + 1; j++) {
-      const horizontalPos = pageWidth / 2;
-      const verticalPos = pageHeight - 12;
-      doc.setPage(j);
-      doc.text(`${j}/${pages}`, horizontalPos, verticalPos, {
-        align: 'center',
+      // example: http://raw.githack.com/MrRio/jsPDF/master/index.html
+      const doc = new jsPDF({
+        orientation: 'portrait',
+        unit: 'px',
       });
-    }
-    this.saveDocLoading = false;
-    if (this.content.title) {
-      const title = this.content.title.replace(/#/i, '');
-      doc.output('dataurlnewwindow', {
-        filename: `${title || 'doc'}.pdf`,
+      doc.setProperties({
+        title: this.content.pdf.fileName || this.content.title,
       });
-    }
-    this.cd.detectChanges();
-    // doc.save('sample.pdf');
+      doc.setFont('fangsong_GB2312');
+      doc.setFontSize(12);
+      autoTable(doc, {
+        startY: 33,
+        tableLineWidth: 0,
+        styles: {
+          font: 'simhei',
+          fontStyle: 'bold',
+          fontSize: 1,
+          fillColor: [255, 255, 255],
+          textColor: [0, 0, 0],
+          valign: 'middle',
+          halign: 'center',
+          lineWidth: 0,
+          cellWidth: 'auto',
+          overflow: 'linebreak',
+        },
+        theme: 'grid',
+        margin: {
+          top: 33,
+          right: 33,
+          bottom: 33,
+          left: 50,
+        },
+        body: pdf.table.header,
+        didDrawCell: (data: any) => {
+          // logo
+          if (
+            data.row.index === 0 &&
+            data.column.index === 0 &&
+            data.cell.section === 'body'
+          ) {
+            const textPos = data.cell.getTextPos();
+            doc.addImage(pdf.logo.data, textPos.x - 53, textPos.y - 8, 107, 16);
+          }
+        },
+      });
+      let finalY = (doc as any).lastAutoTable.finalY;
+
+      autoTable(doc, {
+        startY: finalY,
+        styles: {
+          fillColor: [255, 255, 255],
+          fontSize: 12,
+          font: 'fangsong_GB2312',
+          textColor: [0, 0, 0],
+          valign: 'middle',
+          lineWidth: 0.2,
+          lineColor: 5,
+        },
+        tableLineWidth: 0.2,
+        tableLineColor: 5,
+        margin: {
+          top: 33,
+          right: 33,
+          bottom: 33,
+          left: 50,
+        },
+        theme: 'grid',
+        didDrawCell: (data: any) => {
+          // 签名图片
+          if (
+            data.row.index === pdf.sign.row &&
+            data.column.index === pdf.sign.column &&
+            data.cell.section === 'body'
+          ) {
+            const dim = data.cell.height - data.cell.padding('vertical');
+            const textPos = data.cell.getTextPos();
+            doc.addImage(pdf.sign.data, textPos.x, textPos.y - 7, 47, 14);
+          }
+        },
+        body: pdf.table.body,
+      });
+
+      finalY = (doc as any).lastAutoTable.finalY;
+      const now = Date.now();
+      const time = formatDate(new Date(), 'yyyy年MM月dd日 HH:mm', 'zh-Hans');
+      doc.text(`打印日期：${time}`, 50, finalY + 15);
+
+      // page number
+      const internal: any = doc.internal;
+      const pages = internal.getNumberOfPages();
+      const pageWidth = doc.internal.pageSize.width;
+      const pageHeight = doc.internal.pageSize.height;
+      doc.setFontSize(12);
+
+      for (let j = 1; j < pages + 1; j++) {
+        const horizontalPos = pageWidth / 2;
+        const verticalPos = pageHeight - 12;
+        doc.setPage(j);
+        doc.text(`${j}/${pages}`, horizontalPos, verticalPos, {
+          align: 'center',
+        });
+      }
+      this.saveDocLoading = false;
+      this.cd.detectChanges();
+
+      resolve(doc);
+    });
+  }
+
+  onPreviewPdf(): void {
+    this.buildPdf().then((doc) => {
+      if (this.content.title) {
+        const title = this.content.title.replace(/#/i, '');
+        doc.output('dataurlnewwindow', {
+          filename: `${title || 'doc'}.pdf`,
+        });
+      }
+    });
+  }
+
+  onDownFiles(): void {
+    const today = new Date();
+    this.buildPdf().then((doc) => {
+      doc.save(
+        `${this.content.pdf.fileName}-${formatDate(
+          today,
+          'yyyy-MM-dd',
+          'zh-Hans'
+        )}.pdf`
+      );
+    });
   }
 }
