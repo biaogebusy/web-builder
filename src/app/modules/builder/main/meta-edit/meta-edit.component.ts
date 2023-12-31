a209923d91eca6c6581da0acf70d8d5a06c4287f
biaogebusy <349255833@qq.com>
2023-12-31 22:58:33 +0800

    feat: gui ui builder

--- src/app/modules/builder/main/meta-edit/meta-edit.component.ts
+++ src/app/modules/builder/main/meta-edit/meta-edit.component.ts
@@ -29,9 +29,13 @@ import * as dat from 'dat.gui';
 })
 export class MetaEditComponent implements OnInit, OnDestroy, AfterViewInit {
   @Input() content: any;
-  @ViewChild('gui', { static: false }) gui: ElementRef;
-  guiUI: any;
-  StyleFolder: any;
+  @ViewChild('guiStyle', { static: false }) guiStyle: ElementRef;
+  @ViewChild('guiSize', { static: false }) guiSize: ElementRef;
+  styleGuiUI: any;
+  sizeGuiUI: any;
+  styleFolder: any;
+  sizeFolder: any;
+  viewSpan: any;
   editor: any;
   modules: QuillModule = {
     toolbar: [
@@ -40,16 +44,7 @@ export class MetaEditComponent implements OnInit, OnDestroy, AfterViewInit {
           header: [1, 2, 3, 4, 5, 6, false],
         },
       ],
-      ['bold', 'italic', 'underline', 'strike'],
-      ['clean'],
-      [
-        {
-          background: [],
-        },
-        {
-          color: [],
-        },
-      ],
+      ['bold', 'italic', 'underline'],
       [
         {
           align: [],
@@ -73,6 +68,7 @@ export class MetaEditComponent implements OnInit, OnDestroy, AfterViewInit {
       ],
       ['link', 'image'],
       ['blockquote'],
+      ['clean'],
     ],
   };
   destroy$: Subject<boolean> = new Subject<boolean>();
@@ -113,92 +109,113 @@ export class MetaEditComponent implements OnInit, OnDestroy, AfterViewInit {
     }
 
     const guiSpan = div.querySelector('span');
-    const viewSpan = this.content.ele.querySelector('span') || this.content.ele;
+    this.viewSpan = this.content.ele.querySelector('span') || this.content.ele;
     if (!guiSpan) {
       return;
     }
     guiSpan.style.display = 'inline-block';
+    this.initGuiStyle(guiSpan, this.viewSpan);
+    this.initGuiSize(guiSpan, this.viewSpan);
+  }
+
+  initGuiStyle(guiSpan: HTMLSpanElement, viewSpan: HTMLSpanElement): void {
     // 创建 dat.GUI 实例
-    this.guiUI = new dat.GUI();
+    this.styleGuiUI = new dat.GUI();
 
     const styleParams = {
-      不透明度: parseFloat(guiSpan.style.opacity) || 1,
-      大小: parseFloat(guiSpan.style.fontSize) || 16,
       颜色: guiSpan.style.color || '#222',
-      可见性: guiSpan.style.visibility || 'visible',
+      背景色: guiSpan.style.backgroundColor || '#fff',
+      透明度: parseFloat(guiSpan.style.opacity) || 1,
+      字号: parseFloat(guiSpan.style.fontSize) || 16,
       旋转: 0,
-      对齐: guiSpan.style.textAlign || 'left',
     };
 
-    this.StyleFolder = this.guiUI.addFolder('样式');
-    this.StyleFolder.open();
-    this.StyleFolder.add(styleParams, '不透明度', 0, 1, 0.1).onChange(
-      (value: any) => {
-        viewSpan.style.opacity = value;
-        guiSpan.style.opacity = value;
-        set(
-          this.builder.currentPage.body,
-          this.content.path,
-          guiSpan.outerHTML
-        );
-        this.builder.saveLocalVersions();
-      }
-    );
-    this.StyleFolder.add(styleParams, '大小', 10, 60, 1).onChange(
-      (value: any) => {
-        viewSpan.style.fontSize = `${value}px`;
-        guiSpan.style.fontSize = `${value}px`;
-        set(
-          this.builder.currentPage.body,
-          this.content.path,
-          guiSpan.outerHTML
-        );
-        this.builder.saveLocalVersions();
-      }
-    );
-    this.StyleFolder.addColor(styleParams, '颜色').onChange((value: any) => {
+    this.styleFolder = this.styleGuiUI.addFolder('样式');
+    this.styleFolder.open();
+    this.styleFolder.addColor(styleParams, '颜色').onChange((value: any) => {
       viewSpan.style.color = value;
       guiSpan.style.color = value;
-      set(this.builder.currentPage.body, this.content.path, guiSpan.outerHTML);
-      this.builder.saveLocalVersions();
+      this.setGuiSpan(guiSpan);
     });
-    this.StyleFolder.add(styleParams, '可见性', ['visible', 'hidden']).onChange(
-      (value: any) => {
-        viewSpan.style.visibility = value;
-        guiSpan.style.visibility = value;
-        set(
-          this.builder.currentPage.body,
-          this.content.path,
-          guiSpan.outerHTML
-        );
-        this.builder.saveLocalVersions();
-      }
-    );
-    this.StyleFolder.add(styleParams, '旋转', 0, 360, 1).onChange(
-      (value: any) => {
+    this.styleFolder.addColor(styleParams, '背景色').onChange((value: any) => {
+      viewSpan.style.backgroundColor = value;
+      guiSpan.style.backgroundColor = value;
+      this.setGuiSpan(guiSpan);
+    });
+    this.styleFolder
+      .add(styleParams, '透明度', 0, 1, 0.1)
+      .onChange((value: any) => {
+        viewSpan.style.opacity = value;
+        guiSpan.style.opacity = value;
+        this.setGuiSpan(guiSpan);
+      });
+    this.styleFolder
+      .add(styleParams, '字号', 10, 60, 1)
+      .onChange((value: any) => {
+        viewSpan.style.fontSize = `${value}px`;
+        guiSpan.style.fontSize = `${value}px`;
+        this.setGuiSpan(guiSpan);
+      });
+    this.styleFolder
+      .add(styleParams, '旋转', 0, 360, 1)
+      .onChange((value: any) => {
         viewSpan.style.transform = `rotate(${value}deg)`;
         guiSpan.style.transform = `rotate(${value}deg)`;
-        set(
-          this.builder.currentPage.body,
-          this.content.path,
-          guiSpan.outerHTML
-        );
-        this.builder.saveLocalVersions();
-      }
-    );
+        this.setGuiSpan(guiSpan);
+      });
 
-    this.StyleFolder.add(styleParams, '对齐', [
-      'left',
-      'center',
-      'right',
-    ]).onChange((value: any) => {
-      viewSpan.style.textAlign = value;
-      guiSpan.style.textAlign = value;
-      set(this.builder.currentPage.body, this.content.path, guiSpan.outerHTML);
-      this.builder.saveLocalVersions();
+    this.guiStyle.nativeElement.appendChild(this.styleGuiUI.domElement);
+  }
+
+  initGuiSize(guiSpan: HTMLSpanElement, viewSpan: HTMLSpanElement): void {
+    this.sizeGuiUI = new dat.GUI();
+    const sizeParams = {
+      宽度: guiSpan.style.width || 'auto',
+      高度: guiSpan.style.height || 'auto',
+      行高: guiSpan.style.lineHeight || '1.5',
+      字间距: guiSpan.style.letterSpacing || '0',
+    };
+    this.sizeFolder = this.sizeGuiUI.addFolder('尺寸');
+    this.sizeFolder.open();
+    this.sizeFolder.add(sizeParams, '宽度').onChange((value: any) => {
+      viewSpan.style.width = value;
+      guiSpan.style.width = value;
+      this.setGuiSpan(guiSpan);
+    });
+    this.sizeFolder.add(sizeParams, '高度').onChange((value: any) => {
+      viewSpan.style.height = value;
+      guiSpan.style.height = value;
+      this.setGuiSpan(guiSpan);
+    });
+    this.sizeFolder.add(sizeParams, '行高').onChange((value: any) => {
+      viewSpan.style.lineHeight = value;
+      guiSpan.style.lineHeight = value;
+      this.setGuiSpan(guiSpan);
+    });
+    this.sizeFolder.add(sizeParams, '字间距').onChange((value: any) => {
+      viewSpan.style.letterSpacing = value;
+      guiSpan.style.letterSpacing = value;
+      this.setGuiSpan(guiSpan);
     });
 
-    this.gui.nativeElement.appendChild(this.guiUI.domElement);
+    this.guiSize.nativeElement.appendChild(this.sizeGuiUI.domElement);
+  }
+
+  setGuiSpan(guiSpan: any): void {
+    set(this.builder.currentPage.body, this.content.path, guiSpan.outerHTML);
+    this.builder.saveLocalVersions();
+    this.cd.detectChanges();
+  }
+
+  onClear(): void {
+    const span = this.viewSpan.outerHTML;
+    var spanContent = span.match(/<span[^>]*>([\s\S]+)<\/span>/i);
+    if (spanContent) {
+      this.viewSpan.removeAttribute('style');
+      set(this.builder.currentPage.body, this.content.path, spanContent[1]);
+      this.builder.saveLocalVersions();
+    }
+    this.cd.detectChanges();
   }
 
   isSpanFormatted(str: string): boolean {
@@ -238,7 +255,7 @@ export class MetaEditComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   ngOnDestroy(): void {
-    this.guiUI.removeFolder(this.StyleFolder);
+    this.styleGuiUI.removeFolder(this.styleFolder);
     this.destroy$.next(true);
     this.destroy$.unsubscribe();
   }
