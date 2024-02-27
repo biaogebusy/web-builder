68dfb29374a8792beb13f089b9505791f1ed4172
biaogebusy <349255833@qq.com>
2024-02-27 23:09:43 +0800

    update: builder data

--- src/app/modules/builder/data/widgets-for-builder.ts
+++ src/app/modules/builder/data/widgets-for-builder.ts
@@ -347,9 +347,9 @@ export const widgets = [
       dataset: {
         source: [
           ['红包预算', '2020', '2021', '2022'],
-          ['第一季度', 9568, 3447, 3312],
-          ['第二季度', 4333, 6673, 7435],
-          ['第三季度', 4928, 6104, 4414],
+          ['第一季度', 8630, 7402, 6488],
+          ['第二季度', 4236, 9170, 3201],
+          ['第三季度', 3000, 9405, 6373],
         ],
       },
       xAxis: {
@@ -417,9 +417,9 @@ export const widgets = [
       dataset: {
         source: [
           ['红包预算', '2020', '2021', '2022'],
-          ['第一季度', 9568, 3447, 3312],
-          ['第二季度', 4333, 6673, 7435],
-          ['第三季度', 4928, 6104, 4414],
+          ['第一季度', 8630, 7402, 6488],
+          ['第二季度', 4236, 9170, 3201],
+          ['第三季度', 3000, 9405, 6373],
         ],
       },
       xAxis: {
@@ -493,9 +493,9 @@ export const widgets = [
       dataset: {
         source: [
           ['预算', '费用'],
-          ['大额红包总金额', 819],
-          ['小额红包总金额', 919],
-          ['提成总额', 743],
+          ['大额红包总金额', 583],
+          ['小额红包总金额', 921],
+          ['提成总额', 303],
         ],
       },
       series: [
@@ -549,25 +549,25 @@ export const widgets = [
       elements: [
         {
           module: 'Slick Carousel',
-          tatol: 3591,
+          tatol: 3975,
           info: 'Slick 轮播，强大响应式且性能优异的图片轮播解决方案，有非常丰富的配置选项，slick 官网自己调侃这是你最后使用的图片轮播',
           remarks: '查看',
         },
         {
           module: 'Views Slideshow',
-          tatol: 8723,
+          tatol: 2257,
           info: '视图轮播模块，该模块是最受欢迎的幻灯片模块，可放任何内容，高度定制\t',
           remarks: '查看',
         },
         {
           module: 'Colorbox',
-          tatol: 3726,
+          tatol: 1370,
           info: '轻量级高可定制幻灯片模块，高度集成在 Drupal 中，可放图片，Iframed 或者在线内容等等\t',
           remarks: '查看',
         },
         {
           module: 'Flex Slider',
-          tatol: 6716,
+          tatol: 6152,
           info: '响应式、可调整大小、适配浏览器和移动设备，支持移动设备手势滑动\t',
           remarks: '查看',
         },
@@ -862,7 +862,7 @@ export const widgets = [
             src: '../assets/images/showcase/blog1-large.jpeg',
             alt: '',
           },
-          changed: '2024-02-19',
+          changed: '2024-02-27',
         },
         {
           link: {
@@ -873,29 +873,29 @@ export const widgets = [
             src: '../assets/images/showcase/blog2-large.jpeg',
             alt: '',
           },
-          changed: '2024-02-19',
+          changed: '2024-02-27',
         },
         {
           link: {
             label: '你应该了解的 Angular 最佳实践',
-            href: '2024-02-19',
+            href: '2024-02-27',
           },
           img: {
             src: '../assets/images/showcase/blog3-large.jpeg',
             alt: '',
           },
-          changed: '2024-02-19',
+          changed: '2024-02-27',
         },
         {
           link: {
             label: 'Angular 5 rxjs 5.5.2 多个 http 并行 forkJoin 请求',
-            href: '2024-02-19',
+            href: '2024-02-27',
           },
           img: {
             src: '../assets/images/showcase/blog4-large.jpeg',
             alt: '',
           },
-          changed: '2024-02-19',
+          changed: '2024-02-27',
         },
       ],
     },
