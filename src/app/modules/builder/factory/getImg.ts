cd187c9ec18fe852d88463110d0f1d2daf37caa2
biaogebusy <349255833@qq.com>
2024-03-16 15:43:22 +0800

    feat: img, inlineImg form config

--- src/app/modules/builder/factory/getImg.ts
+++ src/app/modules/builder/factory/getImg.ts
@@ -38,6 +38,109 @@ export function getImg(widget: any, options?: any[]): FormlyFieldConfig[] {
               },
             },
             {
+              key: 'style',
+              fieldGroup: [
+                {
+                  type: 'select',
+                  key: 'aspectRatio',
+                  className: 'width-100',
+                  defaultValue: widget?.style?.aspectRatio || 'auto',
+                  templateOptions: {
+                    label: '宽高比',
+                    options: [
+                      {
+                        label: 'auto',
+                        value: 'auto',
+                      },
+                      {
+                        label: '1:1',
+                        value: '1 / 1',
+                      },
+                      {
+                        label: '1:2',
+                        value: '1 / 2',
+                      },
+                      {
+                        label: '2:1',
+                        value: '2 / 1',
+                      },
+                      {
+                        label: '2:3',
+                        value: '2 / 3',
+                      },
+                      {
+                        label: '3:2',
+                        value: '3 / 2',
+                      },
+                      {
+                        label: '4:3',
+                        value: '4 / 3',
+                      },
+                      {
+                        label: '9:16',
+                        value: '9 / 16',
+                      },
+                      {
+                        label: '16:9',
+                        value: '16 / 9',
+                      },
+                    ],
+                  },
+                },
+                {
+                  type: 'select',
+                  key: 'objectFit',
+                  className: 'width-100',
+                  defaultValue: widget?.style?.objectFit || 'initial',
+                  templateOptions: {
+                    label: '填充方式',
+                    options: [
+                      {
+                        label: 'initial',
+                        value: 'initial',
+                      },
+                      {
+                        label: 'contain',
+                        value: 'contain',
+                      },
+                      {
+                        label: 'cover',
+                        value: 'cover',
+                      },
+                      {
+                        label: 'fill',
+                        value: 'fill',
+                      },
+                      {
+                        label: 'none',
+                        value: 'none',
+                      },
+                      {
+                        label: 'scale-down',
+                        value: 'scale-down',
+                      },
+                      {
+                        label: 'inherit',
+                        value: 'inherit',
+                      },
+                      {
+                        label: 'revert',
+                        value: 'revert',
+                      },
+                      {
+                        label: 'revert-layer',
+                        value: 'revert-layer',
+                      },
+                      {
+                        label: 'unset',
+                        value: 'unset',
+                      },
+                    ],
+                  },
+                },
+              ],
+            },
+            {
               key: 'hostClasses',
               type: 'input',
               defaultValue: widget.hostClasses,
