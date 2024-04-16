434f30b26795f4d022236509ba06febe66b6d5a0
biaogebusy <349255833@qq.com>
2024-04-16 23:15:37 +0800

    quick fix

--- src/stories/drupal/ViewList.stories.ts
+++ src/stories/drupal/ViewList.stories.ts
@@ -84,55 +84,60 @@ Default.args = {
     header,
     form: [
       {
-        type: 'input',
-        key: 'title',
-        templateOptions: {
-          label: '标题',
-        },
-      },
-      {
-        type: 'mat-select',
-        key: 'user',
-        templateOptions: {
-          multiple: true,
-          search: true,
-          hideSelected: true,
-          label: '用户',
-          options: [
-            { value: 1, label: 'Johnson' },
-            { value: 2, label: '表歌' },
-            { value: 3, label: '小南' },
-          ],
-        },
-      },
-      {
-        type: 'date-range',
-        key: 'date',
-        templateOptions: {
-          label: '期间',
-          value: '',
-          placeholder: '选择发布日期',
-        },
+        fieldGroupClassName: 'display-flex flex-wrap',
         fieldGroup: [
           {
             type: 'input',
-            key: 'start',
-            placeholder: '开始',
+            key: 'title',
+            templateOptions: {
+              label: '标题',
+            },
+          },
+          {
+            type: 'mat-select',
+            key: 'user',
+            templateOptions: {
+              multiple: true,
+              search: true,
+              hideSelected: true,
+              label: '用户',
+              options: [
+                { value: 1, label: 'Johnson' },
+                { value: 2, label: '表歌' },
+                { value: 3, label: '小南' },
+              ],
+            },
+          },
+          {
+            type: 'date-range',
+            key: 'date',
+            templateOptions: {
+              label: '期间',
+              value: '',
+              placeholder: '选择发布日期',
+            },
+            fieldGroup: [
+              {
+                type: 'input',
+                key: 'start',
+                placeholder: '开始',
+              },
+              {
+                type: 'input',
+                key: 'end',
+                placeholder: '结束',
+              },
+            ],
           },
           {
             type: 'input',
-            key: 'end',
-            placeholder: '结束',
+            key: 'page',
+            className: 'display-none',
+            templateOptions: {
+              label: '页码',
+            },
           },
         ],
-      },
-      {
-        type: 'input',
-        key: 'page',
-        className: 'display-none',
-        templateOptions: {
-          label: '页码',
-        },
       },
     ],
     data: {
