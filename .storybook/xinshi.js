2e170c4e3f930429a60564c86ba22c640a953174
biaogebusy <349255833@qq.com>
2024-04-17 22:15:58 +0800

    remove storybook

--- .storybook/xinshi.js
+++
@@ -1,9 +0,0 @@
-import { create } from "@storybook/theming";
-
-export default create({
-  base: "light",
-  brandTitle: "信使",
-  brandUrl: "/",
-  brandImage: "/assets/images/logo/logo-blue-storybook.png",
-  brandTarget: "_self",
-});
