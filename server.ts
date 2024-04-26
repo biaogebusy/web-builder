8c75211493b6ec7e7503cf11c2f4a5db81741ec2
biaogebusy <349255833@qq.com>
2024-04-26 17:13:30 +0800

    update core,cli v12

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import 'zone.js/dist/zone-node';
+import 'zone.js/node';
 import { ngExpressEngine } from '@nguniversal/express-engine';
 import express from 'express';
 import { join } from 'path';
