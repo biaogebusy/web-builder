905aae0c2f04abb37401bb8c21283e07ccc34f37
biaogebusy <349255833@qq.com>
2024-05-04 16:45:25 +0800

    enabel xinshi mutil langauage

--- src/environments/environment.xinshi.ts
+++ src/environments/environment.xinshi.ts
@@ -7,4 +7,18 @@ export const environment: IEnvironment = {
   port: 4200,
   cache: true,
   ssr: true,
+  multiLang: true,
+  langs: [
+    {
+      label: '中文',
+      value: 'zh',
+      prefix: '/',
+      default: true,
+    },
+    {
+      label: 'EN',
+      value: 'en',
+      prefix: '/en',
+    },
+  ],
 };
