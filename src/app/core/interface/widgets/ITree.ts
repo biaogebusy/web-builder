ac52f3938ffe7a89d7bf28237a75c952d33a3c62
biaogebusy <349255833@qq.com>
2024-06-08 18:20:48 +0800

    remove: hero1v3,tree-list,packery,showcase2v5,showcase3v2, layout builder build it

--- src/app/core/interface/widgets/ITree.ts
+++
@@ -1,9 +0,0 @@
-export interface ITree {
-  type?: 'tree';
-  label: string;
-  nodes: any[];
-  options?: any;
-  expandAll: boolean;
-  key: string;
-  value?: number;
-}
--- src/app/uiux/combs/masonry/masonry.module.ts
+++ src/app/uiux/combs/masonry/masonry.module.ts
@@ -1,16 +1,14 @@
 import { ComponentFactoryResolver, NgModule } from '@angular/core';
 import { ShareModule } from '@share/share.module';
 import { WidgetsModule } from '../../widgets/widgets.module';
-import { NgxPackeryModule } from 'ngx-packery';
 import { ShuffleComponent } from './shuffle/shuffle.component';
-import { PackeryComponent } from './packery/packery.component';
 import { BaseModule } from '@uiux/base/base.module';
 
-const components = [ShuffleComponent, PackeryComponent];
+const components = [ShuffleComponent];
 
 @NgModule({
   declarations: [...components],
-  imports: [ShareModule, WidgetsModule, NgxPackeryModule],
+  imports: [ShareModule, WidgetsModule],
 
   exports: [...components],
 })
