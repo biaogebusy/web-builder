5680c1c084cf77fb4053d5733f57428f91907749
biaogebusy <349255833@qq.com>
2024-06-21 22:43:17 +0800

    fix: map

--- src/app/core/service/amap.service.ts
+++ src/app/core/service/amap.service.ts
@@ -1,6 +1,5 @@
-import { Injectable } from '@angular/core';
-import AMapLoader from '@amap/amap-jsapi-loader';
-import { Observable, from, Subject, of } from 'rxjs';
+import { Injectable, inject } from '@angular/core';
+import { Observable, Subject, of, BehaviorSubject } from 'rxjs';
 import type { IAmap, IMark, IMarkInfo } from '../interface/IAmap';
 import { ScreenService } from './screen.service';
 
@@ -15,32 +14,33 @@ import { ScreenService } from './screen.service';
 export class AmapService {
   position$ = new Subject();
   markers$: Subject<IMark> = new Subject();
-  mapLoading$: Subject<boolean> = new Subject();
-  constructor(private screenSerivce: ScreenService) {}
+  mapLoading$ = new BehaviorSubject<boolean>(false);
+  screenSerivce = inject(ScreenService);
+  constructor() {}
 
   load(config: IAmap): Observable<any> {
-    // const amapConfig: IAmap = this.coreConfig?.amap;
-    console.log('amapConfig', config);
-    // TODO
-    // if (this.screenSerivce.isPlatformBrowser()) {
-    //   return from(
-    //     AMapLoader.load({
-    //       key: config.key,
-    //       version: config.version,
-    //       plugins: config.plugins, // 需要使用的的插件列表，如比例尺'AMap.Scale'等
-    //       AMapUI: {
-    //         // 是否加载 AMapUI，缺省不加载
-    //         version: '1.1', // AMapUI 缺省 1.1
-    //         plugins: [], // 需要加载的 AMapUI ui插件
-    //       },
-    //       Loca: {
-    //         // 是否加载 Loca， 缺省不加载
-    //         // version: '2.0', // Loca 版本，缺省 1.3.2
-    //       },
-    //     }),
-    //   );
-    // }
-    return of(false);
+    const $map = new BehaviorSubject<object | boolean>(false);
+    if (this.screenSerivce.isPlatformBrowser()) {
+      import('@amap/amap-jsapi-loader').then((AMapLoader) => {
+        AMapLoader.load({
+          key: config.key,
+          version: config.version,
+          plugins: config.plugins, // 需要使用的的插件列表，如比例尺'AMap.Scale'等
+          AMapUI: {
+            // 是否加载 AMapUI，缺省不加载
+            version: '1.1', // AMapUI 缺省 1.1
+            plugins: [], // 需要加载的 AMapUI ui插件
+          },
+          Loca: {
+            // 是否加载 Loca， 缺省不加载
+            // version: '2.0', // Loca 版本，缺省 1.3.2
+          },
+        }).then((amap) => {
+          $map.next(amap);
+        });
+      });
+    }
+    return $map;
   }
 
   getMarker(item: IMarkInfo): any {
