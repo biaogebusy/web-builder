eb782ecead0a83bdc8e12235337ffedfd8321899
biaogebusy <349255833@qq.com>
2024-06-21 10:40:51 +0800

    upgrade storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,6 +6,7 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@chromatic-com/storybook',
   ],
   framework: {
     name: '@storybook/angular',
@@ -19,5 +20,6 @@ const config: StorybookConfig = {
       to: '/assets/mdi.svg',
     },
   ],
+  docs: {},
 };
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -46,6 +46,8 @@ const preview: Preview = {
       },
     },
   },
+
+  tags: ['autodocs']
 };
 
 export default preview;
