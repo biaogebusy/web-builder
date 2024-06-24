ba130803ccd5442963843114521d2b30dd2fb125
biaogebusy <349255833@qq.com>
2024-06-24 16:26:28 +0800

    fix: ssr guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,11 @@ import {
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { APP_INITIALIZER, NgModule, Inject } from '@angular/core';
 import zhHans from '@angular/common/locales/zh-Hans';
-import { HttpClientModule } from '@angular/common/http';
+import {
+  HttpClientModule,
+  provideHttpClient,
+  withFetch,
+} from '@angular/common/http';
 import { CommonModule, DOCUMENT, registerLocaleData } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -74,6 +78,7 @@ import { ComponentService } from '@core/service/component.service';
   ],
   providers: [
     Title,
+    provideHttpClient(withFetch()),
     httpInterceptorProviders,
     CookieService,
     provideClientHydration(),
