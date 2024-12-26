3351513f0162decd21bb818132e84917a784511d
biaogebusy <349255833@qq.com>
2024-12-26 14:28:39 +0800

    fix: form object

--- src/app/modules/builder/main/builder-showcase/builder-showcase.component.ts
+++ src/app/modules/builder/main/builder-showcase/builder-showcase.component.ts
@@ -5,6 +5,7 @@ import { ScreenService } from '@core/service/screen.service';
 import { UtilitiesService } from '@core/service/utilities.service';
 import { BuilderState } from '@core/state/BuilderState';
 import { DialogComponent } from '@uiux/widgets/dialog/dialog.component';
+import { cloneDeep } from 'lodash-es';
 
 @Component({
   selector: 'app-builder-showcase',
@@ -12,38 +13,44 @@ import { DialogComponent } from '@uiux/widgets/dialog/dialog.component';
   styleUrls: ['./builder-showcase.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BuilderShowcaseComponent {
+export class BuilderShowcaseComponent implements OnInit {
   @Input() content: IBuilderShowcase;
-
+  component: any;
   private builder = inject(BuilderState);
   private util = inject(UtilitiesService);
   private dialog = inject(MatDialog);
   private screenService = inject(ScreenService);
 
+  ngOnInit(): void {
+    if (this.screenService.isPlatformBrowser()) {
+      this.component = cloneDeep(this.content.card.components[0]);
+    }
+  }
+
   onClose(): void {
     this.builder.showcase$.next(false);
   }
-  onCopy(component: any): void {
-    this.util.copy(JSON.stringify(component));
-    this.util.openSnackbar(`已复制${component.type}的JSON！`, 'ok');
+  onCopy(): void {
+    this.util.copy(JSON.stringify(this.component));
+    this.util.openSnackbar(`已复制${this.component.type}的JSON！`, 'ok');
   }
 
-  showCode(component: any): void {
+  showCode(): void {
     this.dialog.open(DialogComponent, {
       width: '800px',
       data: {
         inputData: {
           content: {
             type: 'jsoneditor',
-            data: component,
+            data: this.component,
           },
         },
       },
     });
   }
 
-  insert(widget: any): void {
-    this.builder.pushComponent(widget);
+  insert(): void {
+    this.builder.pushComponent(this.component);
     setTimeout(() => {
       const length = this.builder.currentPage.body.length;
       this.screenService.scrollToAnchor(`item-${length - 1}`);
