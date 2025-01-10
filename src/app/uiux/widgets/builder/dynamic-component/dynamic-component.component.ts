aba55cc241f9763d15b21d6649f8a1e382cbfe4c
biaogebusy <349255833@qq.com>
2025-01-10 09:53:19 +0800

    refact: showtoolbar logic

--- src/app/uiux/widgets/builder/dynamic-component/dynamic-component.component.ts
+++ src/app/uiux/widgets/builder/dynamic-component/dynamic-component.component.ts
@@ -1,12 +1,9 @@
 import {
-  AfterContentInit,
   AfterViewInit,
   Component,
   ComponentRef,
-  DestroyRef,
   ElementRef,
   EnvironmentInjector,
-  Inject,
   Input,
   OnChanges,
   OnDestroy,
@@ -17,39 +14,31 @@ import {
   ViewContainerRef,
   createComponent,
   inject,
-  signal,
 } from '@angular/core';
-import { Observable } from 'rxjs';
 import { ScreenService } from '@core/service/screen.service';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { UtilitiesService } from '@core/service/utilities.service';
 import { ComponentService } from '@core/service/component.service';
 import type { IDynamicInputs } from '@core/interface/IAppConfig';
-import { IS_BUILDER_MODE } from '@core/token/token-providers';
 
 @Component({
   selector: 'app-dynamic-component',
   templateUrl: './dynamic-component.component.html',
   styleUrls: ['./dynamic-component.component.scss'],
 })
-export class DynamicComponentComponent
-  implements OnInit, AfterViewInit, OnChanges, OnDestroy, AfterContentInit
-{
+export class DynamicComponentComponent implements OnInit, AfterViewInit, OnChanges, OnDestroy {
   @Input() inputs: IDynamicInputs;
   @Input() index: number; // just for order
+  @Input() showToolbar = false;
   @ViewChild('componentContainer', { read: ViewContainerRef, static: true })
   container: ViewContainerRef;
-  showToolbar = signal(false);
 
   ele = inject(ElementRef);
   util = inject(UtilitiesService);
   screenService = inject(ScreenService);
-  private destroyRef = inject(DestroyRef);
   componentService = inject(ComponentService);
   private readonly renderer = inject(Renderer2);
   private readonly environmentInjector = inject(EnvironmentInjector);
   public componentRef: ComponentRef<unknown> | ComponentRef<any> | undefined | any;
-  constructor(@Inject(IS_BUILDER_MODE) public isBuilderMode$: Observable<boolean>) {}
 
   ngOnInit(): void {}
 
@@ -57,16 +46,6 @@ export class DynamicComponentComponent
     Object.keys(changes).forEach(key => {
       if (!changes[key].firstChange) {
         this.loadComponent();
-      }
-    });
-  }
-
-  ngAfterContentInit(): void {
-    this.isBuilderMode$.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(state => {
-      if (state) {
-        this.showToolbar.set(true);
-      } else {
-        this.showToolbar.set(false);
       }
     });
   }
