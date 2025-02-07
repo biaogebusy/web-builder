72c64cb83552b7e711d77c85463fd2d03291b6c3
biaogebusy <349255833@qq.com>
2025-02-07 11:44:32 +0800

    update port

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { IEnvironment } from '../app/core/interface/IEnvironment';
 export const environment: IEnvironment = {
   apiUrl: 'https://builder.design',
   production: true,
-  port: 4200,
+  port: 4201,
   cache: true,
   multiLang: true,
   langs: [
