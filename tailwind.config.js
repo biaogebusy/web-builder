04b16dab7e9cb6beb3615f20b7a95109e65ddba4
biaogebusy <349255833@qq.com>
2025-03-06 12:02:26 +0800

    fix: maxWidth tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -176,11 +176,6 @@ module.exports = {
       'none': 'none',
       '2': '2 2 0%',
     },
-    maxWidth: {
-      '1/4': '25%',
-      '1/2': '50%',
-      '3/4': '75%',
-    },
   },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
