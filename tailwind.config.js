2ea72cf04e7299fd44eba1d7f034054e3117d51e
biaogebusy <349255833@qq.com>
2025-03-06 12:03:47 +0800

    quick fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,6 @@ module.exports = {
       pattern: /^(flex-(\d{1,2}\/12))$/,
       variants: ['sm', 'md', 'lg'],
     },
-    // 替代列跨度类
     {
       pattern: /^(col-span-([1-9]|1[0-2]))$/,
       variants: ['sm', 'md', 'lg'],
@@ -37,19 +36,14 @@ module.exports = {
       pattern: /^m[xytbrl]?-(1[0-9]|20|[1-9])$/,
       variants: COMMON_VARIANTS,
     },
-    // 布局类
     { pattern: /^(block|inline|flex|grid|hidden|table|flow-root)$/, variants: COMMON_VARIANTS },
     { pattern: /^(absolute|relative|fixed|sticky|static)$/, variants: COMMON_VARIANTS },
     { pattern: /^overflow-(auto|hidden|visible|scroll)$/, variants: COMMON_VARIANTS },
-    { pattern: /^z-([0-9]|10|20|30|40|50|auto)$/ }, // 层级
-
-    // 尺寸类
+    { pattern: /^z-([0-9]|10|20|30|40|50|auto)$/ },
     {
       pattern: /^(w|h)-((1\/[2-4])|full|screen|min|max|auto|[0-9]{1,3})$/,
       variants: COMMON_VARIANTS,
-    }, // 支持 w-1/2, h-full 等
-
-    // 排版类
+    },
     { pattern: /^text-(left|center|right|justify)$/, variants: COMMON_VARIANTS },
     { pattern: /^text-(xs|sm|base|lg|xl|2xl|3xl|4xl|5xl|6xl)$/, variants: COMMON_VARIANTS },
     {
@@ -60,8 +54,6 @@ module.exports = {
       pattern: /^(italic|not-italic|uppercase|lowercase|capitalize|normal-case)$/,
       variants: COMMON_VARIANTS,
     },
-
-    // 颜色类 (按项目主色调调整)
     {
       pattern:
         /^bg-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(100|200|300|400|500|600|700|800|900)$/,
@@ -74,30 +66,22 @@ module.exports = {
       pattern:
         /^border-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(100|200|300|400|500|600|700|800|900)$/,
     },
-
-    // 边框与圆角
     { pattern: /^border(-[tbrl])?-(0|2|4|8)$/, variants: COMMON_VARIANTS },
     {
       pattern: /^rounded(-[tlbr]{1,2})?(|none|sm|md|lg|xl|2xl|3xl|full)$/,
       variants: COMMON_VARIANTS,
     },
-
-    // 阴影与透明度
     { pattern: /^shadow(-sm|md|lg|xl|2xl|none|inner)$/, variants: COMMON_VARIANTS },
     {
       pattern: /^opacity-(0|5|10|20|30|40|50|60|70|75|80|90|95|100)$/,
       variants: COMMON_VARIANTS,
     },
-
-    // 过渡与动画
     {
       pattern: /^transition(-none|all|colors|opacity|shadow|transform)$/,
       variants: COMMON_VARIANTS,
     },
     { pattern: /^duration-(75|100|150|200|300|500|700|1000)$/ },
     { pattern: /^ease-(linear|in|out|in-out)$/ },
-
-    // Flex/Grid 布局
     {
       pattern:
         /^(items|justify|content|place-items|place-content)-(start|end|center|between|around|evenly|stretch)$/,
@@ -108,18 +92,15 @@ module.exports = {
       variants: COMMON_VARIANTS,
     },
     { pattern: /^grid-cols-([1-9]|10|11|12|none)$/, variants: COMMON_VARIANTS },
-    // 新增 translate 相关类
     {
       pattern:
         /^-?translate-[xy]-(0|([1-9]|1[0-9]|20)|(1|2|3|4|5|6|7|8|9|10|11|12)\/(2|3|4|5|6)|full)$/,
       variants: COMMON_VARIANTS,
     },
-    // 新增 line-clamp 类
     {
       pattern: /^line-clamp-(1[0]|[1-9])$/,
       variants: COMMON_VARIANTS,
     },
-    // 尺寸类
     {
       pattern:
         /^(w|h)-(0|full|screen|min|max|sm|fit|px|([1-9]\d?)|1(00|02|04|05|6|8|9)|2[0-9]|3[0-6]|((1[0-2]|[1-9])\/(2|3|4|5|6)))$/,
@@ -128,7 +109,6 @@ module.exports = {
       pattern:
         /^(min-w|max-w|min-h|max-h)-(0|full|screen|min|max|fit|px|([1-9]\d?)|1(00|02|04|05|6|8|9)|2[0-9]|3[0-6]|((1[0-2]|[1-9])\/(2|3|4|5|6))|(xs|sm|md|lg|xl|2xl|3xl|4xl|5xl|6xl|7xl))$/,
     },
-    // ========== SVG 相关配置 ==========
     {
       pattern:
         /^(fill|stroke)-(current|(white|black|slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(100|200|300|400|500|600|700|800|900))$/,
