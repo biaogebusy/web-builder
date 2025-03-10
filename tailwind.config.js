e1f75121c4ab7a9c86e2bcecac72963f1a8beb08
biaogebusy <349255833@qq.com>
2025-03-10 11:42:06 +0800

    update tailwindCss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,59 +19,56 @@ module.exports = {
     'animate-spin',
     'animate-ping',
     'animate-pulse',
-    {
-      pattern: /^(flex-(\d{1,2}\/12))$/,
-      variants: ['sm', 'md', 'lg', 'hover'],
-    },
-    {
-      pattern: /^(col-span-([1-9]|1[0-2]))$/,
-      variants: ['sm', 'md', 'lg', 'hover'],
-    },
-    {
-      pattern: /^p[xytbrl]?-(1[0-9]|20|[1-9])$/,
-      variants: ['sm', 'md', 'lg', 'hover'],
-    },
-    {
-      pattern: /^m[xytbrl]?-(1[0-9]|20|[1-9])$/,
-      variants: ['sm', 'md', 'lg', 'hover'],
-    },
-    {
-      pattern: /^(block|inline|flex|grid|hidden|table|flow-root)$/,
-      variants: ['sm', 'md', 'lg', 'hover'],
-    },
-    { pattern: /^(absolute|relative|fixed|sticky|static)$/, variants: ['sm', 'md', 'lg', 'hover'] },
-    { pattern: /^overflow-(auto|hidden|visible|scroll)$/, variants: ['sm', 'md', 'lg', 'hover'] },
+    { pattern: /^(flex-(\d{1,2}\/12))$/, variants: ['sm', 'md', 'lg'] },
+    { pattern: /^(col-span-([1-9]|1[0-2]))$/, variants: ['sm', 'md', 'lg'] },
+    { pattern: /^p[xytbrl]?-(1[0-9]|20|[1-9])$/, variants: ['sm', 'md', 'lg'] },
+    { pattern: /^m[xytbrl]?-(1[0-9]|20|[1-9])$/, variants: ['sm', 'md', 'lg'] },
+    { pattern: /^(block|inline|flex|grid|hidden|table|flow-root)$/, variants: ['sm', 'md', 'lg'] },
+    { pattern: /^(absolute|relative|fixed|sticky|static)$/, variants: ['sm', 'md', 'lg'] },
+    { pattern: /^overflow-(auto|hidden|visible|scroll)$/, variants: ['sm', 'md', 'lg'] },
     { pattern: /^z-([0-9]|10|20|30|40|50|auto)$/ },
     {
       pattern: /^(w|h)-((1\/[2-4])|full|screen|min|max|auto|[0-9]{1,3})$/,
-      variants: ['sm', 'md', 'lg', 'hover'],
+      variants: ['sm', 'md', 'lg'],
     },
-    { pattern: /^text-(left|center|right|justify)$/, variants: ['sm', 'md', 'lg', 'hover'] },
-    {
-      pattern: /^text-(xs|sm|base|lg|xl|2xl|3xl|4xl|5xl|6xl)$/,
-      variants: ['sm', 'md', 'lg', 'hover'],
-    },
+    { pattern: /^text-(left|center|right|justify)$/, variants: ['sm', 'md', 'lg'] },
+    { pattern: /^text-(xs|sm|base|lg|xl|2xl|3xl|4xl|5xl|6xl)$/, variants: ['sm', 'md', 'lg'] },
     {
       pattern: /^font-(thin|light|normal|medium|semibold|bold|black)$/,
-      variants: ['sm', 'md', 'lg', 'hover'],
+      variants: ['sm', 'md', 'lg'],
     },
     {
       pattern: /^(italic|not-italic|uppercase|lowercase|capitalize|normal-case)$/,
-      variants: ['sm', 'md', 'lg', 'hover'],
+      variants: ['sm', 'md', 'lg'],
     },
     {
       pattern:
         /^bg-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(100|200|300|400|500|600|700|800|900)$/,
+      variants: ['sm', 'md', 'lg', 'hover'],
     },
     {
       pattern:
         /^text-(white|black|current|inherit|transparent|slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(100|200|300|400|500|600|700|800|900)$/,
+      variants: ['sm', 'md', 'lg', 'hover'],
     },
     {
       pattern:
         /^border-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(100|200|300|400|500|600|700|800|900)$/,
+      variants: ['sm', 'md', 'lg', 'hover'],
     },
     { pattern: /^border(-[tbrl])?-(0|2|4|8)$/, variants: ['sm', 'md', 'lg', 'hover'] },
+    {
+      pattern: /^border-(solid|dashed|dotted|double|none)$/,
+      variants: ['sm', 'md', 'lg', 'hover'],
+    },
+    {
+      pattern: /^divide-(solid|dashed|dotted|double|none)$/,
+      variants: ['sm', 'md', 'lg', 'hover'],
+    },
+    {
+      pattern: /^outline-(solid|dashed|dotted|double|none)$/,
+      variants: ['sm', 'md', 'lg', 'hover'],
+    },
     {
       pattern: /^rounded(-[tlbr]{1,2})?(|none|sm|md|lg|xl|2xl|3xl|full)$/,
       variants: ['sm', 'md', 'lg', 'hover'],
@@ -90,22 +87,19 @@ module.exports = {
     {
       pattern:
         /^(items|justify|content|place-items|place-content)-(start|end|center|between|around|evenly|stretch)$/,
-      variants: ['sm', 'md', 'lg', 'hover'],
+      variants: ['sm', 'md', 'lg'],
     },
     {
       pattern: /^gap-([0-9]|10|12|14|16|20|24|28|32|36|40|48|56|64|72|80|96)$/,
-      variants: ['sm', 'md', 'lg', 'hover'],
+      variants: ['sm', 'md', 'lg'],
     },
-    { pattern: /^grid-cols-([1-9]|10|11|12|none)$/, variants: ['sm', 'md', 'lg', 'hover'] },
+    { pattern: /^grid-cols-([1-9]|10|11|12|none)$/, variants: ['sm', 'md', 'lg'] },
     {
       pattern:
         /^-?translate-[xy]-(0|([1-9]|1[0-9]|20)|(1|2|3|4|5|6|7|8|9|10|11|12)\/(2|3|4|5|6)|full)$/,
       variants: ['sm', 'md', 'lg', 'hover'],
     },
-    {
-      pattern: /^line-clamp-(1[0]|[1-9])$/,
-      variants: ['sm', 'md', 'lg', 'hover'],
-    },
+    { pattern: /^line-clamp-(1[0]|[1-9])$/, variants: ['sm', 'md', 'lg'] },
     {
       pattern:
         /^(w|h)-(0|full|screen|min|max|sm|fit|px|([1-9]\d?)|1(00|02|04|05|6|8|9)|2[0-9]|3[0-6]|((1[0-2]|[1-9])\/(2|3|4|5|6)))$/,
@@ -117,43 +111,22 @@ module.exports = {
     {
       pattern:
         /^(fill|stroke)-(current|(white|black|slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(100|200|300|400|500|600|700|800|900))$/,
-      variants: ['sm', 'md', 'lg', 'hover'],
     },
-    {
-      pattern: /^stroke-(0|1|2|3|4|5)$/,
-      variants: ['sm', 'md', 'lg', 'hover'],
-    },
-    {
-      pattern: /^(fill-none|stroke-none)$/,
-    },
-    {
-      pattern: /^bg-gradient-to-(t|tr|r|br|b|bl|l|tl)$/,
-    },
+    { pattern: /^stroke-(0|1|2|3|4|5)$/ },
+    { pattern: /^(fill-none|stroke-none)$/ },
+    { pattern: /^bg-gradient-to-(t|tr|r|br|b|bl|l|tl)$/ },
     {
       pattern:
         /^from-(transparent|current|(white|black|slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(100|200|300|400|500|600|700|800|900))$/,
-      variants: ['sm', 'md', 'lg', 'hover'],
     },
     {
       pattern:
         /^to-(transparent|current|(white|black|slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(100|200|300|400|500|600|700|800|900))$/,
-      variants: ['sm', 'md', 'lg', 'hover'],
     },
   ],
   theme: {
-    screens: {
-      sm: '600px',
-      md: '960px',
-      lg: '1280px',
-      xl: '1470px',
-    },
-    extend: {
-      strokeWidth: {
-        3: '3',
-        4: '4',
-        5: '5',
-      },
-    },
+    screens: { sm: '600px', md: '960px', lg: '1280px', xl: '1470px' },
+    extend: { strokeWidth: { 3: '3', 4: '4', 5: '5' } },
     flex: {
       '1/12': '0 0 8.33%',
       '2/12': '0 0 16.66%',
@@ -180,7 +153,5 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
   ],
-  corePlugins: {
-    preflight: false,
-  },
+  corePlugins: { preflight: false },
 };
