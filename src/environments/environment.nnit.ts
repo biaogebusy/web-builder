174e52c6dffb51643efcdc08361cf8532d090e0e
biaogebusy <349255833@qq.com>
2025-03-17 21:50:36 +0800

    remove no need environent

--- src/environments/environment.nnit.ts
+++
@@ -1,8 +0,0 @@
-import { IEnvironment } from '../app/core/interface/IEnvironment';
-
-export const environment: IEnvironment = {
-  apiUrl: 'https://www.nnitpai.com',
-  production: true,
-  port: 4201,
-  cache: true,
-};
