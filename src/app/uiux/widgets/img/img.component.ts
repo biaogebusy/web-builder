86f0c050a01423c9e89533c4080be4662e4ebc09
biaogebusy <349255833@qq.com>
2025-03-22 12:41:44 +0800

    update img

--- src/app/uiux/widgets/img/img.component.ts
+++ src/app/uiux/widgets/img/img.component.ts
@@ -1,27 +1,19 @@
-import { Component, HostBinding, Input, OnInit, inject } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
 import type { IImg } from '@core/interface/widgets/IImg';
-import type { ICoreConfig } from '@core/interface/IAppConfig';
-import { ScreenService } from '@core/service/screen.service';
-import { CORE_CONFIG } from '@core/token/token-providers';
 
 @Component({
-    selector: 'app-img',
-    templateUrl: './img.component.html',
-    styleUrls: ['./img.component.scss'],
-    standalone: false
+  selector: 'app-img',
+  templateUrl: './img.component.html',
+  styleUrls: ['./img.component.scss'],
+  standalone: false,
 })
 export class ImgComponent implements OnInit {
-  coreConfig = inject<ICoreConfig>(CORE_CONFIG);
-
   @Input() content: IImg | undefined;
   @HostBinding('class') hostClasses: any;
-  screenService = inject(ScreenService);
 
   ngOnInit(): void {
-    if (this.screenService.isPlatformBrowser()) {
-      if (this.content?.hostClasses) {
-        this.hostClasses = this.content.hostClasses;
-      }
+    if (this.content?.hostClasses) {
+      this.hostClasses = this.content.hostClasses;
     }
   }
 }
