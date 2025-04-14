427eff03cc5f4bbbb2c15a465dacfd353bdcb402
biaogebusy <349255833@qq.com>
2025-04-14 18:04:56 +0800

    fix: mat-select

--- src/app/core/pipe/safe-html.pipe.ts
+++ src/app/core/pipe/safe-html.pipe.ts
@@ -2,10 +2,10 @@ import { Pipe, PipeTransform, inject } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ScreenService } from '@core/service/screen.service';
 import DOMPurify from 'dompurify';
-import { isString } from 'lodash';
+import { isString } from 'lodash-es';
 @Pipe({
-    name: 'safeHtml',
-    standalone: false
+  name: 'safeHtml',
+  standalone: false,
 })
 export class SafeHtmlPipe implements PipeTransform {
   private screenService = inject(ScreenService);
