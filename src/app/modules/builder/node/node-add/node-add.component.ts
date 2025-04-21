8ab1f0e8623ef89b0231a14914404176ecf02d74
biaogebusy <349255833@qq.com>
2025-04-21 17:17:03 +0800

    feat: node add

--- src/app/modules/builder/node/node-add/node-add.component.ts
+++ src/app/modules/builder/node/node-add/node-add.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, inject, signal } from '@angular/core';
+import { UntypedFormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { FormlyFieldConfig } from '@ngx-formly/core';
 
 @Component({
   selector: 'app-node-add',
@@ -10,10 +12,40 @@ import { ActivatedRoute } from '@angular/router';
 export class NodeAddComponent implements OnInit {
   public type = signal<string>('');
   private activateRoute = inject(ActivatedRoute);
-
+  public form: UntypedFormGroup = new UntypedFormGroup({});
+  public fields = signal<FormlyFieldConfig[]>([]);
   ngOnInit() {
     this.activateRoute.params.subscribe(params => {
-      this.type.set(params['type']);
+      const type = params['type'];
+      this.type.set(type);
+
+      switch (type) {
+        case 'json':
+          this.fields.set([
+            {
+              key: 'title',
+              type: 'input',
+              props: {
+                label: '标题',
+                required: true,
+              },
+            },
+            {
+              key: 'body',
+              type: 'textarea',
+              props: {
+                label: 'JSON',
+                required: true,
+                rows: 10,
+              },
+            },
+          ]);
+          break;
+      }
     });
+  }
+
+  onSubmit(value: any): void {
+    console.log(value);
   }
 }
