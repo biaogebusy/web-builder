c220f0dc10647467b798cbfb9dba647f35570590
biaogebusy <349255833@qq.com>
2025-06-17 22:29:10 +0800

    refact: serve.ts

--- server.ts
+++ server.ts
@@ -1,9 +1,7 @@
-import { APP_BASE_HREF } from '@angular/common';
-import { CommonEngine } from '@angular/ssr/node';
+import { AngularNodeAppEngine, writeResponseToNodeResponse } from '@angular/ssr/node';
 import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
-import AppServerModule from './src/main.server';
 import { environment } from 'src/environments/environment';
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
@@ -12,7 +10,7 @@ export function app(): express.Express {
   const browserDistFolder = resolve(serverDistFolder, '../browser');
   const indexHtml = join(serverDistFolder, 'index.server.html');
 
-  const commonEngine = new CommonEngine();
+  const angularApp = new AngularNodeAppEngine();
 
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
@@ -28,19 +26,11 @@ export function app(): express.Express {
   );
 
   // All regular routes use the Angular engine
-  server.get('*', (req, res, next) => {
-    const { protocol, originalUrl, baseUrl, headers } = req;
-    commonEngine
-      .render({
-        bootstrap: AppServerModule,
-        documentFilePath: indexHtml,
-        inlineCriticalCss: false,
-        url: `${protocol}://${headers.host}${originalUrl}`,
-        publicPath: browserDistFolder,
-        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
-      })
-      .then(html => res.send(html))
-      .catch(err => next(err));
+  server.get('/**', (req, res, next) => {
+    angularApp
+      .handle(req)
+      .then(response => (response ? writeResponseToNodeResponse(response, res) : next()))
+      .catch(next);
   });
 
   return server;
