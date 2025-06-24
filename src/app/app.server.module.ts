e4bb5242aca50a1e1e1581f4cc7f4c8d69451e83
biaogebusy <349255833@qq.com>
2025-06-24 11:55:30 +0800

    fix: ssr

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { withRoutes, provideServerRendering } from '@angular/ssr';
+import { serverRoutes } from './app.routes.server';
 
 @NgModule({
-  imports: [AppModule, ServerModule],
+  imports: [AppModule],
   bootstrap: [AppComponent],
+  providers: [provideServerRendering(withRoutes(serverRoutes))],
 })
 export class AppServerModule {}
