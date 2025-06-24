15e7e02a41648ebea5db02fe7eb92505fbadab68
biaogebusy <349255833@qq.com>
2025-06-24 12:13:11 +0800

    update log

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 import { withRoutes, provideServerRendering } from '@angular/ssr';
