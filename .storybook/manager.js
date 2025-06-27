14807c2988c59e61bc6462ed616c967820d00864
biaogebusy <349255833@qq.com>
2025-06-27 17:16:35 +0800

    remove storybook to other repo

--- .storybook/manager.js
+++
@@ -1,6 +0,0 @@
-import { addons } from '@storybook/manager-api';
-import xinshi from './xinshi';
-
-addons.setConfig({
-  theme: xinshi,
-});
--- .storybook/xinshi.js
+++
@@ -1,9 +0,0 @@
-import { create } from '@storybook/theming/create';
-
-export default create({
-  base: 'light',
-  brandTitle: '信使',
-  brandUrl: '/',
-  brandImage: '/assets/images/logo/logo-blue-storybook.png',
-  brandTarget: '_self',
-});
