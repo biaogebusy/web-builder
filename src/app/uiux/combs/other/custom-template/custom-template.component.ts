ac4adbfe5a24f1f4a416860a03826f1b4e4a70d5
biaogebusy <349255833@qq.com>
2025-07-13 23:05:33 +0800

    quick fix

--- src/app/uiux/combs/other/custom-template/custom-template.component.ts
+++ src/app/uiux/combs/other/custom-template/custom-template.component.ts
@@ -45,10 +45,10 @@ export class CustomTemplateComponent implements AfterViewInit {
       if (isAPI && api) {
         this.fetchContent('');
       } else {
-        if (json) {
+        try {
           this.renderView(json, html);
           this.pager.set(null);
-        } else {
+        } catch (e) {
           this.renderView(
             {},
             `<div class="m-5 p-5 bg-red-100 rounded-lg">意外错误，请检查配置。</div>`
