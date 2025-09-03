9cf1c98783f58ca05fe7873a4533a0cad679b96f
biaogebusy <349255833@qq.com>
2025-09-03 16:33:47 +0800

    fix: swiper

--- src/app/uiux/combs/carousel/swiper/swiper.component.ts
+++ src/app/uiux/combs/carousel/swiper/swiper.component.ts
@@ -8,7 +8,8 @@ import {
   OnChanges,
   SimpleChanges,
   DestroyRef,
-  output
+  output,
+  afterNextRender,
 } from '@angular/core';
 
 import { Subject } from 'rxjs';
@@ -55,6 +56,28 @@ export class SwiperComponent implements OnInit, AfterViewInit, OnChanges {
     ],
   };
   private config: any;
+
+  constructor() {
+    afterNextRender(() => {
+      const swiperEle = this.swiper.nativeElement;
+      Object.assign(swiperEle, this.config);
+      swiperEle.initialize();
+      swiperEle.classList.add(this.content?.classes);
+      swiperEle.addEventListener('swiperslidechange', (event: any) => {
+        const [swiper] = event.detail;
+        this.slideChange.emit(swiper);
+      });
+      if (this.navigationSub) {
+        this.navigationSub.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(action => {
+          if (action > 0) {
+            swiperEle.swiper.slideNext();
+          } else {
+            swiperEle.swiper.slidePrev();
+          }
+        });
+      }
+    });
+  }
   ngOnInit(): void {
     let customPagination = {};
     if (this.content?.custom?.pagination?.bulletsStyle) {
@@ -71,22 +94,6 @@ export class SwiperComponent implements OnInit, AfterViewInit, OnChanges {
   async ngAfterViewInit(): Promise<void> {
     if (this.screenService.isPlatformBrowser()) {
       await this.util.loadStyle('/assets/injects/swiper/swiper-bundle.min.css');
-      const swiperEle = this.swiper.nativeElement;
-      Object.assign(swiperEle, this.config);
-      swiperEle.initialize();
-      swiperEle.addEventListener('swiperslidechange', (event: any) => {
-        const [swiper] = event.detail;
-        this.slideChange.emit(swiper);
-      });
-      if (this.navigationSub) {
-        this.navigationSub.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(action => {
-          if (action > 0) {
-            swiperEle.swiper.slideNext();
-          } else {
-            swiperEle.swiper.slidePrev();
-          }
-        });
-      }
     }
   }
 
