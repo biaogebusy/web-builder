5a134ec3f64c96abd9205020d75eb140d40e574c
biaogebusy <349255833@qq.com>
2025-11-02 11:09:25 +0800

    feat: cover signal iframe

--- src/app/uiux/combs/other/iframe/iframe.component.ts
+++ src/app/uiux/combs/other/iframe/iframe.component.ts
@@ -2,36 +2,35 @@ import {
   Component,
   Input,
   OnInit,
-  ChangeDetectorRef,
   ChangeDetectionStrategy,
   inject,
   DestroyRef,
+  signal,
 } from '@angular/core';
 import type { IUser } from '@core/interface/IUser';
 import type { IIframe } from '@core/interface/widgets/IWidgets';
 import { PAGE_CONTENT, USER } from '@core/token/token-providers';
 import { ScreenService } from '@core/service/screen.service';
 import { IPage } from '@core/interface/IAppConfig';
-import { Observable } from 'rxjs';
+import { Observable, single } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
-    selector: 'app-iframe',
-    templateUrl: './iframe.component.html',
-    styleUrls: ['./iframe.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: false
+  selector: 'app-iframe',
+  templateUrl: './iframe.component.html',
+  styleUrls: ['./iframe.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: false,
 })
 export class IframeComponent implements OnInit {
   private user$ = inject<Observable<IUser>>(USER);
   private pageContent$ = inject<Observable<IPage>>(PAGE_CONTENT);
 
   @Input() content: IIframe;
-  url: string;
-  loading: boolean;
-  user: IUser;
-  cd = inject(ChangeDetectorRef);
-  screenService = inject(ScreenService);
+  public url = signal('');
+  public loading = signal(false);
+  private user: IUser;
+  private screenService = inject(ScreenService);
   private destroyRef = inject(DestroyRef);
 
   constructor() {
@@ -43,20 +42,16 @@ export class IframeComponent implements OnInit {
   ngOnInit(): void {
     if (this.screenService.isPlatformBrowser()) {
       if (this.content.url.includes('disable_sidebar')) {
-        this.loading = true;
+        this.loading.set(true);
         window.addEventListener(
           'message',
           event => {
             if (event.data === 'ready') {
-              this.loading = false;
-              console.log('ready');
-              this.cd.detectChanges();
+              this.loading.set(false);
             }
 
             if (event.data === 'loading') {
-              this.loading = true;
-              console.log('loading');
-              this.cd.detectChanges();
+              this.loading.set(true);
             }
           },
           false
@@ -64,15 +59,13 @@ export class IframeComponent implements OnInit {
       }
     }
     if (!this.content?.url.includes('/:')) {
-      this.url = this.content.url;
-      this.cd.detectChanges();
+      this.url.set(this.content.url);
       return;
     }
     if (this.content?.url.includes(':id')) {
       if (this.user) {
         const id = this.user.current_user.uid;
-        this.url = this.content.url.replace(':id', id);
-        this.cd.detectChanges();
+        this.url.set(this.content.url.replace(':id', id));
       }
     }
 
@@ -81,12 +74,11 @@ export class IframeComponent implements OnInit {
         const nid = page.config?.node?.nid;
         if (nid) {
           // node 的详情页
-          this.url = this.content.url.replace(':nid', nid);
+          this.url.set(this.content.url.replace(':nid', nid));
         } else {
           // 非node详情页，例如日历页面
-          this.url = '/go-to-node';
+          this.url.set('/go-to-node');
         }
-        this.cd.detectChanges();
       });
     }
   }
