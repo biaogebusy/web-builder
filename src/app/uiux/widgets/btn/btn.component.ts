1cb09dc5d8aecf8a05c3420195e20827efe736dd
biaogebusy <349255833@qq.com>
2025-11-22 19:02:57 +0800

    feat: btn disabled event

--- src/app/uiux/widgets/btn/btn.component.ts
+++ src/app/uiux/widgets/btn/btn.component.ts
@@ -1,17 +1,29 @@
-import { ChangeDetectionStrategy, Component, Input, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  Input,
+  OnInit,
+  inject,
+} from '@angular/core';
 import type { IBtn } from '@core/interface/widgets/IBtn';
 import { RouteService } from '@core/service/route.service';
 
 @Component({
-    selector: 'app-btn',
-    templateUrl: './btn.component.html',
-    styleUrls: ['./btn.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: false
+  selector: 'app-btn',
+  templateUrl: './btn.component.html',
+  styleUrls: ['./btn.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: false,
 })
-export class BtnComponent {
+export class BtnComponent implements OnInit {
   @Input() content: IBtn;
   routeService = inject(RouteService);
+  @HostBinding('class.disabled') disabled = false;
+
+  ngOnInit(): void {
+    this.disabled = this.content.disabled ?? false;
+  }
 
   onNav(event: any, content: any): void {
     this.routeService.toNavigate(event, content);
